41afe6392c8f9019df761a7f347dfacc7279ed6d
yeon-06 <rnjstldus2@gmail.com>
2022-06-28 15:50:24 +0900

    feat: 백엔드 프로젝트 초기 환경 설정

---
+++ backend/src/test/java/com/pickpick/PickpickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pickpick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickpickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
