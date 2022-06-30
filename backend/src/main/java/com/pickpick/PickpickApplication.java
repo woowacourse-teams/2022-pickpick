12eaa322ab2e1c818c5eaa535ae93ef8063166fd
yeonLog <53105735+yeon-06@users.noreply.github.com>
2022-06-30 10:19:29 +0900

    feat: 백엔드 프로젝트 초기 환경 설정 (#3)
    * docs: 템플릿 버전 개선 #1
    
    * feat: 백엔드 프로젝트 초기 환경 설정
    
    Co-authored-by: hyewoncc <80666066+hyewoncc@users.noreply.github.com>

---
+++ backend/src/main/java/com/pickpick/PickpickApplication.java
@@ -0,0 +1,13 @@
+package com.pickpick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickpickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickpickApplication.class, args);
+	}
+
+}
