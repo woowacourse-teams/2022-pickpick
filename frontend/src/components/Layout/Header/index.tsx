d7c90a2542cad9353bb1dd08ef8fcfac613f43dd
kkojae <jaejeung3210@gmail.com>
2022-07-05 17:11:49 +0900

    feat: Layout component 개발
    - Header, Footer, Navigation, LayoutContainer UI components 개발
    - theme type 설정
    
    Co-authored-by: moonheekim0118 <hellomooneekim@gmail.com>

---
+++ frontend/src/components/Layout/Header/index.tsx
@@ -0,0 +1,12 @@
+import * as Styled from "./style";
+
+function Header() {
+  return (
+    <Styled.Container>
+      <Styled.Title>줍줍</Styled.Title>
+      <button type="button">로그인</button>
+    </Styled.Container>
+  );
+}
+
+export default Header;
