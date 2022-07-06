2342c76299d013af78e0e594fb170e946a6eea3a
hyewoncc <hyewoncc@gmail.com>
2022-07-06 15:16:09 +0900

    fix: unix time 변환 유틸 오류 수정

--- backend/src/main/java/com/pickpick/utils/TimeUtils.java
+++ backend/src/main/java/com/pickpick/utils/TimeUtils.java
@@ -3,20 +3,17 @@ package com.pickpick.utils;
 import java.math.BigDecimal;
 import java.time.Instant;
 import java.time.LocalDateTime;
-import java.util.TimeZone;
+import java.time.ZoneId;
 
 public class TimeUtils {
     private TimeUtils() {
     }
 
-    public static LocalDateTime toLocalDateTime(long unixTime) {
-        return LocalDateTime.ofInstant(
-                Instant.ofEpochMilli(unixTime),
-                TimeZone.getDefault().toZoneId()
-        );
-    }
-
     public static LocalDateTime toLocalDateTime(String unixTime) {
         return toLocalDateTime(new BigDecimal(unixTime).longValue());
+    }
+
+    private static LocalDateTime toLocalDateTime(long unixTime) {
+        return LocalDateTime.ofInstant(Instant.ofEpochSecond(unixTime), ZoneId.systemDefault());
     }
 }
