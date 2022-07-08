404436498ea35d1d527cd584d9de1e45b3854855
moonheekim0118 <hellomooneekim@gmail.com>
2022-07-08 10:46:07 +0900

    feat: Home Page MSW 모킹 (#41)
    * chore: MSW 설치 및 설정
    - MSW 설치
    - 초기 handlers 정의 및 모킹 환경 세팅
    - Message Mock Data 정의
    
    * feat: HomePage Message 데이터 MSW 연동
    
    * fix: Mock data 오타 수정
    
    * refactor: util 폴더 내부 파일명 수정
    - @utils/utils -> @utils/index
    
    * refactor: mock data 파일명 수정
    - mockData -> data
    
    * refactor: package.json 내 EOL 제거

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -6,6 +6,12 @@ import Home from "@src/pages/Home";
 import LayoutContainer from "@src/components/layouts/LayoutContainer";
 import GlobalStyle from "./@styles/GlobalStyle";
 
+if (process.env.NODE_ENV === "development") {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const { worker } = require("./mocks/browser");
+  worker.start();
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root") as Element);
 
 root.render(
