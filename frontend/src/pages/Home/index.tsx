937691913326d4790d3fc35f42075669a214ccc5
kkojae <jaejeung3210@gmail.com>
2022-07-08 11:31:58 +0900

    refactor: useQuery 적용
    - loading, error 처리 추가
    - useEffect 내부 로직 useQuery 변경
    
    Co-authored-by: moonheekim0118 <hellomooneekim@gmail.com>

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -3,16 +3,29 @@ import Dropdown from "@src/components/Dropdown";
 import MessageCard from "@src/components/MessageCard";
 import SearchInput from "@src/components/SearchInput";
 import * as Styled from "./style";
-import { useEffect, useState } from "react";
+import { useQuery } from "react-query";
+import { getMessages } from "@src/api/messages";
+
+interface Message {
+  id: string;
+  username: string;
+  postedDate: string;
+  text: string;
+  userThumbnail: string;
+}
+
+interface Response {
+  messages: Message[];
+}
 
 function Home() {
-  const [messages, setMessages] = useState([]);
+  const { data, isLoading, isError } = useQuery<Response>(
+    "messages",
+    getMessages
+  );
 
-  useEffect(() => {
-    fetch("/api/messages")
-      .then((response) => response.json())
-      .then(({ messages }) => setMessages(messages));
-  }, []);
+  if (isLoading) return <div>로딩중이야!!!</div>;
+  if (isError) return <div>이거슨 에러양!!!!</div>;
 
   return (
     <Styled.Container>
@@ -21,15 +34,17 @@ function Home() {
         <Dropdown />
       </Styled.Wrapper>
       <FlexColumn gap="4px">
-        {messages.map(({ id, username, postedDate, text, userThumbnail }) => (
-          <MessageCard
-            key={id}
-            username={username}
-            date={postedDate}
-            text={text}
-            thumbnail={userThumbnail}
-          />
-        ))}
+        {data?.messages.map(
+          ({ id, username, postedDate, text, userThumbnail }) => (
+            <MessageCard
+              key={id}
+              username={username}
+              date={postedDate}
+              text={text}
+              thumbnail={userThumbnail}
+            />
+          )
+        )}
       </FlexColumn>
     </Styled.Container>
   );
