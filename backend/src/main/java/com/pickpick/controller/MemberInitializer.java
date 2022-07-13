4780040b28364f89b8a92b56dacff0530506521d
bomi <bonita9931@gmail.com>
2022-07-13 19:20:14 +0900

    feat: 존재하는 유저를 저장 대상에서 제외

--- backend/src/main/java/com/pickpick/controller/MemberInitializer.java
+++ backend/src/main/java/com/pickpick/controller/MemberInitializer.java
@@ -6,14 +6,19 @@ import com.slack.api.Slack;
 import com.slack.api.methods.MethodsClient;
 import com.slack.api.methods.SlackApiException;
 import com.slack.api.model.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
 import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
-import javax.annotation.PostConstruct;
-import org.springframework.stereotype.Component;
 
 @Component
 public class MemberInitializer {
+
+    @Value("${slack.bot-token}")
+    private String slackBotToken;
 
     private final MemberRepository memberRepository;
 
@@ -23,15 +28,22 @@ public class MemberInitializer {
 
     @PostConstruct
     void setupMember() throws SlackApiException, IOException {
-        memberRepository.deleteAll();
+        List<String> existSlackIds = memberRepository.findAll()
+                .stream()
+                .map(Member::getSlackId)
+                .collect(Collectors.toList());
 
         MethodsClient client = Slack.getInstance().methods();
 
-        final List<Member> members = toPickpickMembers(
-                client.usersList(r -> r.token("xoxb-3740298320131-3740551023445-ZYo41nYYQxrGPRT0BWnSB5dU"))
+        List<Member> foundMembers = toPickpickMembers(
+                client.usersList(r -> r.token(slackBotToken))
                         .getMembers());
 
-        memberRepository.saveAll(members);
+        List<Member> membersToSave = foundMembers.stream()
+                .filter(it -> !existSlackIds.contains(it.getSlackId()))
+                .collect(Collectors.toList());
+
+        memberRepository.saveAll(membersToSave);
     }
 
     private List<Member> toPickpickMembers(final List<User> users) {
--- backend/src/main/java/com/pickpick/repository/MemberRepository.java
+++ backend/src/main/java/com/pickpick/repository/MemberRepository.java
@@ -1,9 +1,10 @@
 package com.pickpick.repository;
 
 import com.pickpick.entity.Member;
+import org.springframework.data.repository.Repository;
+
 import java.util.List;
 import java.util.Optional;
-import org.springframework.data.repository.Repository;
 
 public interface MemberRepository extends Repository<Member, Long> {
     Optional<Member> findBySlackId(String slackId);
