42fb9c34554368f85cbddfb24c5f74a63a3dc264
kkojae <jaejeung3210@gmail.com>
2022-07-14 21:27:36 +0900

    feat: Navigation component에 Portal 추가
    - handleToggleDrawer, handleCloseDrawer 핸들러 함수 생성 및 적용
    - overflowY 설정
    
    Co-authored-by: moonheekim0118 <hellomooneekim@gmail.com>

--- frontend/src/components/layouts/Navigation/index.tsx
+++ frontend/src/components/layouts/Navigation/index.tsx
@@ -1,15 +1,37 @@
 import * as Styled from "./style";
+import { useState, useEffect } from "react";
 import MenuIcon from "@public/assets/icons/MenuIcon.svg";
 import StarIconUnfill from "@public/assets/icons/StarIcon-Unfill.svg";
 import HomeIconUnfill from "@public/assets/icons/HomeIcon-Unfill.svg";
 import AlarmIconInactive from "@public/assets/icons/AlarmIcon-Inactive.svg";
 import InfoIcon from "@public/assets/icons/InfoIcon.svg";
 import WrapperButton from "@src/components/@shared/WrapperButton";
+import Dimmer from "@src/components/@shared/Dimmer";
+import Portal from "@src/components/@shared/Portal";
+import Drawer from "@src/components/Drawer";
 
 function Navigation() {
+  const [isMenuDrawerOpened, setIsMenuDrawerOpened] = useState(false);
+
+  const handleCloseDrawer = () => {
+    setIsMenuDrawerOpened(false);
+  };
+
+  const handleToggleDrawer = () => {
+    setIsMenuDrawerOpened((prev) => !prev);
+  };
+
+  useEffect(() => {
+    if (isMenuDrawerOpened) {
+      document.body.style.overflowY = "hidden";
+      return;
+    }
+    document.body.style.overflowY = "auto";
+  }, [isMenuDrawerOpened]);
+
   return (
     <Styled.Container>
-      <WrapperButton kind="bigIcon">
+      <WrapperButton kind="bigIcon" onClick={handleToggleDrawer}>
         <MenuIcon width="24px" height="24px" fill="#121212" />
       </WrapperButton>
       <WrapperButton kind="bigIcon">
@@ -24,6 +46,13 @@ function Navigation() {
       <WrapperButton kind="bigIcon">
         <InfoIcon width="24px" height="24px" fill="#121212" />
       </WrapperButton>
+
+      <Portal isOpened={isMenuDrawerOpened}>
+        <>
+          <Dimmer hasBackgroundColor={true} onClick={handleCloseDrawer} />
+          <Drawer />
+        </>
+      </Portal>
     </Styled.Container>
   );
 }
