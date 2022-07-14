fff685dfbfd9e5913e7ef5c5a2fccd3d58d2fea8
bomi <bonita9931@gmail.com>
2022-07-14 14:17:46 +0900

    refactor: MemberInitializer 패키지 이동 및 메서드 추출

--- backend/src/main/java/com/pickpick/service/MemberInitializer.java
+++ backend/src/main/java/com/pickpick/service/MemberInitializer.java
@@ -1,4 +1,4 @@
-package com.pickpick.controller;
+package com.pickpick.service;
 
 import com.pickpick.entity.Member;
 import com.pickpick.repository.MemberRepository;
@@ -20,6 +20,7 @@ public class MemberInitializer {
     @Value("${slack.bot-token}")
     private String slackBotToken;
 
+    private final MethodsClient client = Slack.getInstance().methods();
     private final MemberRepository memberRepository;
 
     public MemberInitializer(final MemberRepository memberRepository) {
@@ -28,31 +29,39 @@ public class MemberInitializer {
 
     @PostConstruct
     void setupMember() throws SlackApiException, IOException {
-        List<String> existSlackIds = memberRepository.findAll()
-                .stream()
-                .map(Member::getSlackId)
-                .collect(Collectors.toList());
-
-        MethodsClient client = Slack.getInstance().methods();
-
-        List<Member> foundMembers = toPickpickMembers(
-                client.usersList(r -> r.token(slackBotToken))
-                        .getMembers());
-
-        List<Member> membersToSave = foundMembers.stream()
-                .filter(it -> !existSlackIds.contains(it.getSlackId()))
-                .collect(Collectors.toList());
+        List<String> savedSlackIds = findSavedSlackIds();
+        List<Member> currentWorkspaceMembers = getCurrentWorkspaceMembers();
+        List<Member> membersToSave = filterMembersToSave(savedSlackIds, currentWorkspaceMembers);
 
         memberRepository.saveAll(membersToSave);
     }
 
-    private List<Member> toPickpickMembers(final List<User> users) {
-        return users.stream()
-                .map(this::toPickpickMember)
+    private List<String> findSavedSlackIds() {
+        return memberRepository.findAll()
+                .stream()
+                .map(Member::getSlackId)
                 .collect(Collectors.toList());
     }
 
-    private Member toPickpickMember(final User user) {
+    private List<Member> getCurrentWorkspaceMembers() throws IOException, SlackApiException {
+        return toMembers(
+                client.usersList(request -> request.token(slackBotToken))
+                        .getMembers());
+    }
+
+    private List<Member> filterMembersToSave(final List<String> savedSlackIds, final List<Member> currentWorkspaceMembers) {
+        return currentWorkspaceMembers.stream()
+                .filter(it -> !savedSlackIds.contains(it.getSlackId()))
+                .collect(Collectors.toList());
+    }
+
+    private List<Member> toMembers(final List<User> users) {
+        return users.stream()
+                .map(this::toMember)
+                .collect(Collectors.toList());
+    }
+
+    private Member toMember(final User user) {
         return new Member(user.getId(), user.getProfile().getDisplayName(), user.getProfile().getImage512());
     }
 }
