af3cf2149d2326ddab6dcdd05fe2bc94ce1e700f
Richard JEON <ztzy1907@gmail.com>
2022-07-16 12:52:56 +0900

    style: 개행 추가

--- backend/src/main/java/com/pickpick/exception/MessageNotFoundException.java
+++ backend/src/main/java/com/pickpick/exception/MessageNotFoundException.java
@@ -1,6 +1,7 @@
 package com.pickpick.exception;
 
 public class MessageNotFoundException extends RuntimeException {
+
     private static final String DEFAULT_MESSAGE = "메시지를 찾지 못했습니다";
 
     public MessageNotFoundException() {
