90a5ecf5a6a6471f5c899963dfc4f54d2db61a5b
kkojae <jaejeung3210@gmail.com>
2022-07-16 16:16:40 +0900

    refactor: mocking data, handlers 수정
    Co-authored-by: moonheekim0118 <hellomooneekim@gmail.com>

--- frontend/src/mocks/handlers.ts
+++ frontend/src/mocks/handlers.ts
@@ -3,16 +3,20 @@ import { messages } from "./data";
 
 export const handlers = [
   rest.get("/api/messages", (req, res, ctx) => {
-    const page = Number(req.url.searchParams.get("page"));
+    const messageId = Number(req.url.searchParams.get("messageId"));
     const size = Number(req.url.searchParams.get("size"));
 
-    const slicedMessages = messages.slice((page - 1) * size, size * page);
+    const targetIndex =
+      messages.findIndex((message) => message.id === messageId) + 1;
+
+    const newMessages = messages.slice(targetIndex, size + targetIndex);
 
     return res(
+      ctx.status(200),
+      ctx.delay(500),
       ctx.json({
-        messages: slicedMessages,
-        isLast: slicedMessages.length !== 20,
-        nextPage: page + 1,
+        messages: newMessages,
+        isLast: newMessages.length !== size,
       })
     );
   }),
