ca1200f265813d72f00e7c772c52b4d7004f6a1d
Richard JEON <ztzy1907@gmail.com>
2022-07-16 14:12:01 +0900

    test: 메시지 조회 테스트 가독성 개선

--- backend/src/test/java/com/pickpick/service/MessageServiceTest.java
+++ backend/src/test/java/com/pickpick/service/MessageServiceTest.java
@@ -15,8 +15,12 @@ import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestConstructor;
 import org.springframework.test.context.TestConstructor.AutowireMode;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.transaction.annotation.Transactional;
 
+@Sql("/message.sql")
 @TestConstructor(autowireMode = AutowireMode.ALL)
+@Transactional
 @SpringBootTest
 class MessageServiceTest {
 
@@ -26,13 +30,12 @@ class MessageServiceTest {
         this.messageService = messageService;
     }
 
-    @ParameterizedTest
-    @MethodSource("slackMessageRequest")
     @DisplayName("메시지 조회 요청에 따른 메시지가 응답된다")
+    @MethodSource("slackMessageRequest")
+    @ParameterizedTest(name = "{0}")
     void findMessages(
-            final SlackMessageRequest slackMessageRequest,
-            final List<Long> expectedMessageIds,
-            final boolean expectedLast) {
+            final String description, final SlackMessageRequest slackMessageRequest,
+            final List<Long> expectedMessageIds, final boolean expectedLast) {
         // given
         SlackMessageResponses slackMessageResponses = messageService.find(slackMessageRequest);
 
@@ -50,6 +53,7 @@ class MessageServiceTest {
     private static Stream<Arguments> slackMessageRequest() {
         return Stream.of(
                 Arguments.of(
+                        "1번, 2번 채널에서 메시지ID가 1인 메시지 이후에 작성된 메시지 7개 조회",
                         new SlackMessageRequest(null, null, List.of(1L, 2L), false, 1L, 7),
                         List.of(222L, 3L, 4L, 5L, 6L, 7L, 8L),
                         true)
