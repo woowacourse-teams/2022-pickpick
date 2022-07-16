96a57d4bdc2bf258ce1f02d71365017bed07c506
kkojae <jaejeung3210@gmail.com>
2022-07-16 16:18:16 +0900

    refactor: messages api 수정
    Co-authored-by: moonheekim0118 <hellomooneekim@gmail.com>

--- frontend/src/api/messages.ts
+++ frontend/src/api/messages.ts
@@ -1,9 +1,11 @@
 import { ResponseMessages } from "@src/@types/shared";
 import { fetcher } from ".";
 
-export const getMessages = async ({ pageParam = 1 }) => {
+// TODO: 백엔드에 초기 페이지 디폴트값 어떻게 구분할지  물어보기
+// query ''
+export const getMessages = async ({ pageParam = 0 }) => {
   const { data } = await fetcher.get<ResponseMessages>(
-    `/api/messages?page=${pageParam}&size=20`
+    `/api/messages?messageId=${pageParam}&size=20`
   );
 
   return data;
