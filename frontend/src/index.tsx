e4a5c5c8e69d021e53f37877dea0a4eb663d80ad
kkojae <jaejeung3210@gmail.com>
2022-07-16 16:16:26 +0900

    fix: 페이지 진입시 재렌더링 되는 문제 해결
    - react query default option 추가
    
    Co-authored-by: moonheekim0118 <hellomooneekim@gmail.com>

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -17,7 +17,13 @@ if (process.env.NODE_ENV === "development") {
 
 const root = ReactDOM.createRoot(document.getElementById("root") as Element);
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 root.render(
   <BrowserRouter>
