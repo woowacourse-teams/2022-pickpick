2f95cd75080d8011fa796d08ae52e1995939b87b
kkojae <jaejeung3210@gmail.com>
2022-07-17 15:35:47 +0900

    refactor: getMessages API 수정
    Co-authored-by: moonheekim0118 <hellomooneekim@gmail.com>

--- frontend/src/api/messages.ts
+++ frontend/src/api/messages.ts
@@ -1,18 +1,12 @@
 import { ResponseMessages } from "@src/@types/shared";
 import { fetcher } from ".";
 
-export const getNextMessages = async ({ pageParam = 0 }) => {
-  const { data } = await fetcher.get<ResponseMessages>(
-    `/api/messages?messageId=${pageParam}&size=20&needPastMessage=true`
-  );
+export const getMessages =
+  ({ date = "", needPastMessage = true } = {}) =>
+  async ({ pageParam = 0 }) => {
+    const { data } = await fetcher.get<ResponseMessages>(
+      `/api/messages?messageId=${pageParam}&size=20&needPastMessage=${needPastMessage}&date=${date}`
+    );
 
-  return data;
-};
-
-export const getPreviousMessages = async ({ pageParam = 0 }) => {
-  const { data } = await fetcher.get<ResponseMessages>(
-    `/api/messages?messageId=${pageParam}&size=20&needPastMessage=false`
-  );
-
-  return data;
-};
+    return data;
+  };
