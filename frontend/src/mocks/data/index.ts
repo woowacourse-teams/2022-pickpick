83d8ef728d191b13311d859219e155cb6b7a6250
kkojae <jaejeung3210@gmail.com>
2022-07-18 16:25:46 +0900

    refactor: mocking handler 중복 로직 제거 및 mocking data 수정
    Co-authored-by: moonheekim0118 <hellomooneekim@gmail.com>

--- frontend/src/mocks/data/index.ts
+++ frontend/src/mocks/data/index.ts
@@ -1,6 +1,286 @@
 export const messages = [
   {
-    id: 123,
+    id: 12711,
+    userId: 1,
+    username: "엘보우(송상민)",
+    userThumbnail:
+      "https://ca.slack-edge.com/TFELTJB7V-U02V8MLHTEW-d31d9f3b8609-512",
+    text: "엘보우 바보1",
+    postedDate: "2022-07-18T14:50:58.972493",
+    modifiedDate: "2022-07-18T14:56:58.972493",
+  },
+  {
+    id: 12712,
+    userId: 1,
+    username: "호프(김문희)",
+    userThumbnail:
+      "https://ca.slack-edge.com/TFELTJB7V-U02V8MLHTEW-d31d9f3b8609-512",
+    text: "호프 바보2",
+    postedDate: "2022-07-18T14:50:58.972493",
+    modifiedDate: "2022-07-18T14:56:58.972493",
+  },
+  {
+    id: 12713,
+    userId: 1,
+    username: "호프(김문희)",
+    userThumbnail:
+      "https://ca.slack-edge.com/TFELTJB7V-U02V8MLHTEW-d31d9f3b8609-512",
+    text: "호프 바보4",
+    postedDate: "2022-07-18T14:50:58.972493",
+    modifiedDate: "2022-07-18T14:56:58.972493",
+  },
+  {
+    id: 12715,
+    userId: 1,
+    username: "호프(김문희)",
+    userThumbnail:
+      "https://ca.slack-edge.com/TFELTJB7V-U02V8MLHTEW-d31d9f3b8609-512",
+    text: "호프 바보5",
+    postedDate: "2022-07-18T14:50:58.972493",
+    modifiedDate: "2022-07-18T14:56:58.972493",
+  },
+  {
+    id: 12716,
+    userId: 1,
+    username: "호프(김문희)",
+    userThumbnail:
+      "https://ca.slack-edge.com/TFELTJB7V-U02V8MLHTEW-d31d9f3b8609-512",
+    text: "호프 바보6",
+    postedDate: "2022-07-18T14:50:58.972493",
+    modifiedDate: "2022-07-18T14:56:58.972493",
+  },
+  {
+    id: 12717,
+    userId: 1,
+    username: "호프(김문희)",
+    userThumbnail:
+      "https://ca.slack-edge.com/TFELTJB7V-U02V8MLHTEW-d31d9f3b8609-512",
+    text: "호프 바보7",
+    postedDate: "2022-07-18T14:50:58.972493",
+    modifiedDate: "2022-07-18T14:56:58.972493",
+  },
+  {
+    id: 12718,
+    userId: 1,
+    username: "호프(김문희)",
+    userThumbnail:
+      "https://ca.slack-edge.com/TFELTJB7V-U02V8MLHTEW-d31d9f3b8609-512",
+    text: "호프 바보8",
+    postedDate: "2022-07-18T14:50:58.972493",
+    modifiedDate: "2022-07-18T14:56:58.972493",
+  },
+  {
+    id: 12719,
+    userId: 1,
+    username: "호프(김문희)",
+    userThumbnail:
+      "https://ca.slack-edge.com/TFELTJB7V-U02V8MLHTEW-d31d9f3b8609-512",
+    text: "호프 바보9",
+    postedDate: "2022-07-18T14:50:58.972493",
+    modifiedDate: "2022-07-18T14:56:58.972493",
+  },
+  {
+    id: 12722,
+    userId: 1,
+    username: "호프(김문희)",
+    userThumbnail:
+      "https://ca.slack-edge.com/TFELTJB7V-U02V8MLHTEW-d31d9f3b8609-512",
+    text: "호프 바보10",
+    postedDate: "2022-07-18T14:50:58.972493",
+    modifiedDate: "2022-07-18T14:56:58.972493",
+  },
+  {
+    id: 12723,
+    userId: 1,
+    username: "호프(김문희)",
+    userThumbnail:
+      "https://ca.slack-edge.com/TFELTJB7V-U02V8MLHTEW-d31d9f3b8609-512",
+    text: "호프 바보11",
+    postedDate: "2022-07-18T14:50:58.972493",
+    modifiedDate: "2022-07-18T14:56:58.972493",
+  },
+  {
+    id: 12724,
+    userId: 1,
+    username: "호프(김문희)",
+    userThumbnail:
+      "https://ca.slack-edge.com/TFELTJB7V-U02V8MLHTEW-d31d9f3b8609-512",
+    text: "호프 바보12",
+    postedDate: "2022-07-18T14:50:58.972493",
+    modifiedDate: "2022-07-18T14:56:58.972493",
+  },
+  {
+    id: 12725,
+    userId: 1,
+    username: "호프(김문희)",
+    userThumbnail:
+      "https://ca.slack-edge.com/TFELTJB7V-U02V8MLHTEW-d31d9f3b8609-512",
+    text: "호프 바보13",
+    postedDate: "2022-07-18T14:50:58.972493",
+    modifiedDate: "2022-07-18T14:56:58.972493",
+  },
+  {
+    id: 12726,
+    userId: 1,
+    username: "호프(김문희)",
+    userThumbnail:
+      "https://ca.slack-edge.com/TFELTJB7V-U02V8MLHTEW-d31d9f3b8609-512",
+    text: "호프 바보14",
+    postedDate: "2022-07-18T14:50:58.972493",
+    modifiedDate: "2022-07-18T14:56:58.972493",
+  },
+  {
+    id: 12727,
+    userId: 1,
+    username: "호프(김문희)",
+    userThumbnail:
+      "https://ca.slack-edge.com/TFELTJB7V-U02V8MLHTEW-d31d9f3b8609-512",
+    text: "호프 바보15",
+    postedDate: "2022-07-18T14:50:58.972493",
+    modifiedDate: "2022-07-18T14:56:58.972493",
+  },
+  {
+    id: 1274,
+    userId: 1,
+    username: "호프(김문희)",
+    userThumbnail:
+      "https://ca.slack-edge.com/TFELTJB7V-U02V8MLHTEW-d31d9f3b8609-512",
+    text: "호프 바보",
+    postedDate: "2022-07-18T14:50:58.972493",
+    modifiedDate: "2022-07-18T14:56:58.972493",
+  },
+  {
+    id: 1275,
+    userId: 1,
+    username: "호프(김문희)",
+    userThumbnail:
+      "https://ca.slack-edge.com/TFELTJB7V-U02V8MLHTEW-d31d9f3b8609-512",
+    text: "호프 바보",
+    postedDate: "2022-07-18T14:50:58.972493",
+    modifiedDate: "2022-07-18T14:56:58.972493",
+  },
+  {
+    id: 1276,
+    userId: 1,
+    username: "호프(김문희)",
+    userThumbnail:
+      "https://ca.slack-edge.com/TFELTJB7V-U02V8MLHTEW-d31d9f3b8609-512",
+    text: "호프 바보",
+    postedDate: "2022-07-18T14:50:58.972493",
+    modifiedDate: "2022-07-18T14:56:58.972493",
+  },
+  {
+    id: 1277,
+    userId: 1,
+    username: "호프(김문희)",
+    userThumbnail:
+      "https://ca.slack-edge.com/TFELTJB7V-U02V8MLHTEW-d31d9f3b8609-512",
+    text: "호프 바보",
+    postedDate: "2022-07-18T14:50:58.972493",
+    modifiedDate: "2022-07-18T14:56:58.972493",
+  },
+  {
+    id: 1278,
+    userId: 1,
+    username: "호프(김문희)",
+    userThumbnail:
+      "https://ca.slack-edge.com/TFELTJB7V-U02V8MLHTEW-d31d9f3b8609-512",
+    text: "호프 바보",
+    postedDate: "2022-07-18T14:50:58.972493",
+    modifiedDate: "2022-07-18T14:56:58.972493",
+  },
+  {
+    id: 1279,
+    userId: 1,
+    username: "호프(김문희)",
+    userThumbnail:
+      "https://ca.slack-edge.com/TFELTJB7V-U02V8MLHTEW-d31d9f3b8609-512",
+    text: "호프 바보",
+    postedDate: "2022-07-18T14:50:58.972493",
+    modifiedDate: "2022-07-18T14:56:58.972493",
+  },
+  {
+    id: 1212,
+    userId: 1,
+    username: "호프(김문희)",
+    userThumbnail:
+      "https://ca.slack-edge.com/TFELTJB7V-U02V8MLHTEW-d31d9f3b8609-512",
+    text: "호프 바보",
+    postedDate: "2022-07-18T14:50:58.972493",
+    modifiedDate: "2022-07-18T14:56:58.972493",
+  },
+  {
+    id: 1211,
+    userId: 1,
+    username: "호프(김문희)",
+    userThumbnail:
+      "https://ca.slack-edge.com/TFELTJB7V-U02V8MLHTEW-d31d9f3b8609-512",
+    text: "호프 바보",
+    postedDate: "2022-07-18T14:50:58.972493",
+    modifiedDate: "2022-07-18T14:56:58.972493",
+  },
+  {
+    id: 1213,
+    userId: 1,
+    username: "호프(김문희)",
+    userThumbnail:
+      "https://ca.slack-edge.com/TFELTJB7V-U02V8MLHTEW-d31d9f3b8609-512",
+    text: "호프 바보",
+    postedDate: "2022-07-18T14:50:58.972493",
+    modifiedDate: "2022-07-18T14:56:58.972493",
+  },
+  {
+    id: 1214,
+    userId: 1,
+    username: "호프(김문희)",
+    userThumbnail:
+      "https://ca.slack-edge.com/TFELTJB7V-U02V8MLHTEW-d31d9f3b8609-512",
+    text: "호프 바보",
+    postedDate: "2022-07-18T14:50:58.972493",
+    modifiedDate: "2022-07-18T14:56:58.972493",
+  },
+  {
+    id: 127,
+    userId: 1,
+    username: "호프(김문희)",
+    userThumbnail:
+      "https://ca.slack-edge.com/TFELTJB7V-U02V8MLHTEW-d31d9f3b8609-512",
+    text: "호프 바보",
+    postedDate: "2022-07-18T14:50:58.972493",
+    modifiedDate: "2022-07-18T14:56:58.972493",
+  },
+  {
+    id: 126,
+    userId: 1,
+    username: "호프(김문희)",
+    userThumbnail:
+      "https://ca.slack-edge.com/TFELTJB7V-U02V8MLHTEW-d31d9f3b8609-512",
+    text: "호프 바보",
+    postedDate: "2022-07-18T14:50:58.972493",
+    modifiedDate: "2022-07-18T14:56:58.972493",
+  },
+  {
+    id: 125,
+    userId: 1,
+    username: "호프(김문희)",
+    userThumbnail:
+      "https://ca.slack-edge.com/TFELTJB7V-U02V8MLHTEW-d31d9f3b8609-512",
+    text: "호프 바보",
+    postedDate: "2022-07-18T14:50:58.972493",
+    modifiedDate: "2022-07-18T14:56:58.972493",
+  },
+  {
+    id: 1299,
+    userId: 1,
+    username: "호프(김문희)",
+    userThumbnail:
+      "https://ca.slack-edge.com/TFELTJB7V-U02V8MLHTEW-d31d9f3b8609-512",
+    text: "호프 바보 여기여기여기여기기가 어제 처음시작이야!",
+    postedDate: "2022-07-17T14:50:58.972493",
+    modifiedDate: "2022-07-17T14:56:58.972493",
+  },
+  {
+    id: 1290,
     userId: 1,
     username: "호프(김문희)",
     userThumbnail:
@@ -10,12 +290,122 @@ export const messages = [
     modifiedDate: "2022-07-17T14:56:58.972493",
   },
   {
-    id: 119,
+    id: 1291,
     userId: 1,
     username: "호프(김문희)",
     userThumbnail:
       "https://ca.slack-edge.com/TFELTJB7V-U02V8MLHTEW-d31d9f3b8609-512",
-    text: "안녕하세요. 1 안녕하세요. 1 안녕하세요. 1 안녕하세요. 1 안녕하세요. 1 안녕하세요. 1 안녕하세요. 1 안녕하세요. 1 안녕하세요. 1 안녕하세요. 1 안녕하세요. 1 안녕하세요. 1 안녕하세요. 1 안녕하세요. 1 안녕하세요. 1 안녕하세요. 1 안녕하세요. 1 안녕하세요. 1 안녕하세요. 1 안녕하세요. 1 안녕하세요. 1 안녕하세요. 1 안녕하세요. 1 안녕하세요. 1 안녕하세요. 1 안녕하세요. 1 안녕하세요. 1 안녕하세요. 1 안녕하세요. 1 안녕하세요. 1 안녕하세요. 1 안녕하세요. 1 안녕하세요. 1 ",
+    text: "호프 바보",
+    postedDate: "2022-07-17T14:50:58.972493",
+    modifiedDate: "2022-07-17T14:56:58.972493",
+  },
+  {
+    id: 1292,
+    userId: 1,
+    username: "호프(김문희)",
+    userThumbnail:
+      "https://ca.slack-edge.com/TFELTJB7V-U02V8MLHTEW-d31d9f3b8609-512",
+    text: "호프 바보",
+    postedDate: "2022-07-17T14:50:58.972493",
+    modifiedDate: "2022-07-17T14:56:58.972493",
+  },
+  {
+    id: 1293,
+    userId: 1,
+    username: "호프(김문희)",
+    userThumbnail:
+      "https://ca.slack-edge.com/TFELTJB7V-U02V8MLHTEW-d31d9f3b8609-512",
+    text: "호프 바보",
+    postedDate: "2022-07-17T14:50:58.972493",
+    modifiedDate: "2022-07-17T14:56:58.972493",
+  },
+  {
+    id: 1294,
+    userId: 1,
+    username: "호프(김문희)",
+    userThumbnail:
+      "https://ca.slack-edge.com/TFELTJB7V-U02V8MLHTEW-d31d9f3b8609-512",
+    text: "호프 바보",
+    postedDate: "2022-07-17T14:50:58.972493",
+    modifiedDate: "2022-07-17T14:56:58.972493",
+  },
+  {
+    id: 1295,
+    userId: 1,
+    username: "호프(김문희)",
+    userThumbnail:
+      "https://ca.slack-edge.com/TFELTJB7V-U02V8MLHTEW-d31d9f3b8609-512",
+    text: "호프 바보",
+    postedDate: "2022-07-17T14:50:58.972493",
+    modifiedDate: "2022-07-17T14:56:58.972493",
+  },
+  {
+    id: 1296,
+    userId: 1,
+    username: "호프(김문희)",
+    userThumbnail:
+      "https://ca.slack-edge.com/TFELTJB7V-U02V8MLHTEW-d31d9f3b8609-512",
+    text: "호프 바보",
+    postedDate: "2022-07-17T14:50:58.972493",
+    modifiedDate: "2022-07-17T14:56:58.972493",
+  },
+  {
+    id: 1297,
+    userId: 1,
+    username: "호프(김문희)",
+    userThumbnail:
+      "https://ca.slack-edge.com/TFELTJB7V-U02V8MLHTEW-d31d9f3b8609-512",
+    text: "ghfhfhfhfhfhdsalkfjasdlkfasjdflkasjdflkasdflkasdjflkasjfdalk",
+    postedDate: "2022-07-16T14:50:58.972493",
+    modifiedDate: "2022-07-16T14:56:58.972493",
+  },
+  {
+    id: 188,
+    userId: 1,
+    username: "호프(김문희)",
+    userThumbnail:
+      "https://ca.slack-edge.com/TFELTJB7V-U02V8MLHTEW-d31d9f3b8609-512",
+    text: "ghfhfhfhfhfhdsalkfjasdlkfasjdflkasjdflkasdflkasdjflkasjfdalk",
+    postedDate: "2022-07-16T14:50:58.972493",
+    modifiedDate: "2022-07-16T14:56:58.972493",
+  },
+  {
+    id: 181,
+    userId: 1,
+    username: "호프(김문희)",
+    userThumbnail:
+      "https://ca.slack-edge.com/TFELTJB7V-U02V8MLHTEW-d31d9f3b8609-512",
+    text: "ghfhfhfhfhfhdsalkfjasdlkfasjdflkasjdflkasdflkasdjflkasjfdalk",
+    postedDate: "2022-07-16T14:50:58.972493",
+    modifiedDate: "2022-07-16T14:56:58.972493",
+  },
+  {
+    id: 182,
+    userId: 1,
+    username: "호프(김문희)",
+    userThumbnail:
+      "https://ca.slack-edge.com/TFELTJB7V-U02V8MLHTEW-d31d9f3b8609-512",
+    text: "ghfhfhfhfhfhdsalkfjasdlkfasjdflkasjdflkasdflkasdjflkasjfdalk",
+    postedDate: "2022-07-16T14:50:58.972493",
+    modifiedDate: "2022-07-16T14:56:58.972493",
+  },
+  {
+    id: 1192,
+    userId: 1,
+    username: "호프(김문희)",
+    userThumbnail:
+      "https://ca.slack-edge.com/TFELTJB7V-U02V8MLHTEW-d31d9f3b8609-512",
+    text: "ghfhfhfhfhfhdsalkfjasdlkfasjdflkasjdflkasdflkasdjflkasjfdalk",
+    postedDate: "2022-07-16T14:50:58.972493",
+    modifiedDate: "2022-07-16T14:56:58.972493",
+  },
+  {
+    id: 1191,
+    userId: 1,
+    username: "호프(김문희)",
+    userThumbnail:
+      "https://ca.slack-edge.com/TFELTJB7V-U02V8MLHTEW-d31d9f3b8609-512",
+    text: "ghfhfhfhfhfhdsalkfjasdlkfasjdflkasjdflkasdflkasdjflkasjfdalk",
     postedDate: "2022-07-16T14:50:58.972493",
     modifiedDate: "2022-07-16T14:56:58.972493",
   },
@@ -206,8 +596,8 @@ export const messages = [
     userThumbnail:
       "https://ca.slack-edge.com/TFELTJB7V-U02V8MLHTEW-d31d9f3b8609-512",
     text: "안녕하세요. 1 안녕하세요. 1 안녕하세요. 1 안녕하세요. 1 안녕하세요. 1 안녕하세요. 1 안녕하세요. 1 안녕하세요. 1 안녕하세요. 1 안녕하세요. 1 안녕하세요. 1 안녕하세요. 1 안녕하세요. 1 안녕하세요. 1 안녕하세요. 1 안녕하세요. 1 안녕하세요. 1 안녕하세요. 1 안녕하세요. 1 안녕하세요. 1 안녕하세요. 1 안녕하세요. 1 안녕하세요. 1 안녕하세요. 1 안녕하세요. 1 안녕하세요. 1 안녕하세요. 1 안녕하세요. 1 안녕하세요. 1 안녕하세요. 1 안녕하세요. 1 안녕하세요. 1 안녕하세요. 1 ",
-    postedDate: "2022-07-15T14:50:58.972493",
-    modifiedDate: "2022-07-15T14:56:58.972493",
+    postedDate: "2022-07-14T14:50:58.972493",
+    modifiedDate: "2022-07-14T14:56:58.972493",
   },
   {
     id: 2,
@@ -216,8 +606,8 @@ export const messages = [
     userThumbnail:
       "https://ca.slack-edge.com/TFELTJB7V-U02V8MLHTEW-d31d9f3b8609-512",
     text: "안녕하세요. 2",
-    postedDate: "2022-07-15T14:50:58.972493",
-    modifiedDate: "2022-07-15T14:56:58.972493",
+    postedDate: "2022-07-14T14:50:58.972493",
+    modifiedDate: "2022-07-14T14:56:58.972493",
   },
   {
     id: 3,
@@ -226,8 +616,18 @@ export const messages = [
     userThumbnail:
       "https://ca.slack-edge.com/TFELTJB7V-U02V8MLHTEW-d31d9f3b8609-512",
     text: "안녕하세요. 3",
-    postedDate: "2022-07-15T14:50:58.972493",
-    modifiedDate: "2022-07-15T14:56:58.972493",
+    postedDate: "2022-07-14T14:50:58.972493",
+    modifiedDate: "2022-07-14T14:56:58.972493",
+  },
+  {
+    id: 4444444,
+    userId: 1,
+    username: "호프(김문희)",
+    userThumbnail:
+      "https://ca.slack-edge.com/TFELTJB7V-U02V8MLHTEW-d31d9f3b8609-512",
+    text: "안녕하세요. 4",
+    postedDate: "2022-07-14T14:50:58.972493",
+    modifiedDate: "2022-07-14T14:56:58.972493",
   },
   {
     id: 4,
@@ -240,7 +640,37 @@ export const messages = [
     modifiedDate: "2022-07-14T14:56:58.972493",
   },
   {
-    id: 5,
+    id: 44,
+    userId: 1,
+    username: "호프(김문희)",
+    userThumbnail:
+      "https://ca.slack-edge.com/TFELTJB7V-U02V8MLHTEW-d31d9f3b8609-512",
+    text: "안녕하세요. 4",
+    postedDate: "2022-07-14T14:50:58.972493",
+    modifiedDate: "2022-07-14T14:56:58.972493",
+  },
+  {
+    id: 43,
+    userId: 1,
+    username: "호프(김문희)",
+    userThumbnail:
+      "https://ca.slack-edge.com/TFELTJB7V-U02V8MLHTEW-d31d9f3b8609-512",
+    text: "안녕하세요. 4",
+    postedDate: "2022-07-14T14:50:58.972493",
+    modifiedDate: "2022-07-14T14:56:58.972493",
+  },
+  {
+    id: 42,
+    userId: 1,
+    username: "호프(김문희)",
+    userThumbnail:
+      "https://ca.slack-edge.com/TFELTJB7V-U02V8MLHTEW-d31d9f3b8609-512",
+    text: "안녕하세요. 4",
+    postedDate: "2022-07-14T14:50:58.972493",
+    modifiedDate: "2022-07-14T14:56:58.972493",
+  },
+  {
+    id: 51,
     userId: 1,
     username: "호프(김문희)",
     userThumbnail:
--- frontend/src/mocks/handlers.ts
+++ frontend/src/mocks/handlers.ts
@@ -1,125 +1,75 @@
-import { rest } from "msw";
+import { DefaultBodyType, ResponseComposition, rest, RestContext } from "msw";
 import { messages, channels, subscribedChannels } from "./data";
 
 const SIZE = 20;
 let standardDate = "";
+
+const getPreviousResponseInfo = (
+  res: ResponseComposition<DefaultBodyType>,
+  ctx: RestContext,
+  targetIndex: number
+) => {
+  const frontIndex = targetIndex - SIZE < 0 ? 0 : targetIndex - SIZE;
+
+  const newMessages = messages.slice(frontIndex, targetIndex);
+
+  return res(
+    ctx.status(200),
+    ctx.delay(500),
+    ctx.json({
+      messages: newMessages,
+      isLast: newMessages.length !== SIZE,
+    })
+  );
+};
+
+const getNextResponseInfo = (
+  res: ResponseComposition<DefaultBodyType>,
+  ctx: RestContext,
+  targetIndex: number
+) => {
+  const newMessages = messages.slice(targetIndex, SIZE + targetIndex);
+
+  return res(
+    ctx.status(200),
+    ctx.delay(500),
+    ctx.json({
+      messages: newMessages,
+      isLast: newMessages.length !== SIZE,
+    })
+  );
+};
 
 export const handlers = [
   rest.get("/api/messages", (req, res, ctx) => {
     const messageId = Number(req.url.searchParams.get("messageId"));
     const needPastMessage =
       req.url.searchParams.get("needPastMessage") === "true";
-    const date = req.url.searchParams.get("date");
+    const date = req.url.searchParams.get("date")?.split("T")[0];
 
-    const targetIndex =
-      messages.findIndex((message) => message.id === messageId) + 1;
+    const targetIndex = messages.findIndex(
+      (message) => message.id === messageId
+    );
 
-    const newMessages = messages.slice(targetIndex, SIZE + targetIndex);
-
-    if (!date) {
-      return res(
-        ctx.status(200),
-        ctx.delay(500),
-        ctx.json({
-          messages: newMessages,
-          isLast: newMessages.length !== SIZE,
-        })
-      );
+    if (!date && !messageId) {
+      return getNextResponseInfo(res, ctx, targetIndex + 1);
     }
 
-    // 같은 리스트를 계속해서 렌더링하는 문제가 있음
-    // 첫번째 요청시에는 date를 기준으로 slice를 쳐야함
-    // 두번째 세번째 ~~ 부터는 배열에 id를 기준으로 slice를 쳐야한다.
+    const dateTargetIndex = messages.findIndex(
+      (message) => message.postedDate.split("T")[0] === date
+    );
 
-    if (standardDate !== date) {
+    if (standardDate !== date && needPastMessage && date) {
       standardDate = date;
 
-      const dateTargetIndex = messages.findIndex(
-        (message) => message.postedDate === date
-      );
-
-      if (needPastMessage) {
-        const newDateMessages = messages.slice(
-          dateTargetIndex,
-          SIZE + dateTargetIndex
-        );
-
-        return res(
-          ctx.status(200),
-          ctx.delay(500),
-          ctx.json({
-            messages: newDateMessages,
-            isLast: newDateMessages.length !== SIZE,
-          })
-        );
-      }
-
-      const frontIndex =
-        dateTargetIndex - SIZE < 0 ? 0 : dateTargetIndex - SIZE;
-
-      const newDateMessages = messages.slice(frontIndex, dateTargetIndex);
-
-      return res(
-        ctx.status(200),
-        ctx.delay(500),
-        ctx.json({
-          messages: newDateMessages,
-          isLast: newDateMessages.length != SIZE,
-        })
-      );
+      return getNextResponseInfo(res, ctx, dateTargetIndex);
     }
 
-    if (standardDate === date) {
-      if (messageId === 0) {
-        const dateTargetIndex = messages.findIndex(
-          (message) => message.postedDate === date
-        );
-
-        const newDateMessages = messages.slice(
-          dateTargetIndex,
-          SIZE + dateTargetIndex
-        );
-
-        return res(
-          ctx.status(200),
-          ctx.delay(500),
-          ctx.json({
-            messages: newDateMessages,
-            isLast: newDateMessages.length !== SIZE,
-          })
-        );
-      }
-
-      const targetIndex = messages.findIndex(
-        (message) => message.id === messageId
-      );
-
-      if (needPastMessage) {
-        const newMessages = messages.slice(targetIndex, SIZE + targetIndex);
-
-        return res(
-          ctx.status(200),
-          ctx.delay(500),
-          ctx.json({
-            messages: newMessages,
-            isLast: newMessages.length !== SIZE,
-          })
-        );
-      }
-
-      const frontIndex = targetIndex - SIZE < 0 ? 0 : targetIndex - SIZE;
-
-      const newMessages = messages.slice(frontIndex, targetIndex);
-
-      return res(
-        ctx.status(200),
-        ctx.delay(500),
-        ctx.json({
-          messages: newMessages,
-          isLast: newMessages.length !== SIZE,
-        })
-      );
+    if (needPastMessage) {
+      return getNextResponseInfo(res, ctx, targetIndex + 1);
     }
+
+    return getPreviousResponseInfo(res, ctx, targetIndex);
   }),
 
   rest.get("/api/channels", (req, res, ctx) => {
