ae8726c3adcb757151542e11b7328324008f9e95
kkojae <jaejeung3210@gmail.com>
2022-07-18 16:31:30 +0900

    refactor: useInfiniteQuery 리팩터링
    - onWheel event 추가
    - useInfiniteQuery 1개로 병합
    - PreviousInfiniteScroll component 제거
    - getNextPageParam, getPreviousPageParam return 값 형식 변경
    
    Co-authored-by: moonheekim0118 <hellomooneekim@gmail.com>

--- frontend/src/pages/SpecificDateFeed/index.tsx
+++ frontend/src/pages/SpecificDateFeed/index.tsx
@@ -1,60 +1,46 @@
 import * as Styled from "../Feed/style";
-import React, { useRef } from "react";
-import PreviousInfiniteScroll from "@src/components/@shared/PreviousInfiniteScroll";
+import React, { useEffect, useRef } from "react";
 import NextInfiniteScroll from "@src/components/@shared/NextInfiniteScroll";
-import { InfiniteData, useInfiniteQuery } from "react-query";
-import { Message, ResponseMessages } from "@src/@types/shared";
+import { useInfiniteQuery } from "react-query";
+import { ResponseMessages } from "@src/@types/shared";
 import { getMessages } from "@src/api/messages";
 import MessageCardSkeleton from "@src/components/MessageCardSkeleton";
 import { FlexColumn } from "@src/@styles/shared";
 import MessageCard from "@src/components/MessageCard";
 import Dropdown from "@src/components/Dropdown";
 import { useParams } from "react-router-dom";
+import SearchInput from "@src/components/SearchInput";
 
 function SpecificDateFeed() {
   const { date } = useParams();
   const dateArrayRef = useRef<string[]>([]);
+  const wheelPosition = useRef({ default: 0, move: 0, scroll: 0 });
+  const flag = useRef(false);
+  const isInitialRender = useRef(true);
 
   const {
-    data: previousData,
-    isLoading: isPreviousDataLoading,
-    isError: isPreviousDataError,
+    data,
+    isFetching,
+    isError,
     fetchPreviousPage,
     hasPreviousPage,
+    fetchNextPage,
+    hasNextPage,
+    refetch,
   } = useInfiniteQuery<ResponseMessages>(
-    ["datePreviousMessages"],
+    ["dateMessages"],
     getMessages({
-      needPastMessage: false,
       date,
     }),
     {
       getPreviousPageParam: ({ isLast, messages }) => {
         if (!isLast) {
-          return messages[0]?.id;
+          return { messageId: messages[0]?.id, needPastMessage: false };
         }
       },
-      onSettled: () => {
-        dateArrayRef.current = [];
-      },
-    }
-  );
-
-  const {
-    data: nextData,
-    isLoading: isNextDataLoading,
-    isError: isNextDataError,
-    fetchNextPage,
-    hasNextPage,
-  } = useInfiniteQuery<ResponseMessages>(
-    ["dateNextMessages"],
-    getMessages({
-      needPastMessage: true,
-      date,
-    }),
-    {
       getNextPageParam: ({ isLast, messages }) => {
         if (!isLast) {
-          return messages.at(-1)?.id;
+          return { messageId: messages.at(-1)?.id, needPastMessage: true };
         }
       },
       onSettled: () => {
@@ -63,19 +49,7 @@ function SpecificDateFeed() {
     }
   );
 
-  if (isPreviousDataError || isNextDataError) return <div>이거슨 에러양!</div>;
-
-  const extractMessages = (
-    previousData?: InfiniteData<ResponseMessages>,
-    nextData?: InfiniteData<ResponseMessages>
-  ): Message[] => {
-    if (!previousData || !nextData) return [];
-
-    return [
-      ...previousData.pages.flatMap((arr) => arr.messages),
-      ...nextData.pages.flatMap((arr) => arr.messages),
-    ];
-  };
+  if (isError) return <div>이거슨 에러양!</div>;
 
   const renderDateDropdown = (postedDate: string) => {
     if (dateArrayRef.current.includes(postedDate)) return;
@@ -89,53 +63,90 @@ function SpecificDateFeed() {
     );
   };
 
+  const onWheel = (event: any) => {
+    wheelPosition.current.move = event.deltaY;
+
+    if (
+      wheelPosition.current.move < -10 &&
+      !flag.current &&
+      wheelPosition.current.scroll < 200
+    ) {
+      hasPreviousPage && fetchPreviousPage();
+      flag.current = true;
+
+      return;
+    }
+
+    if (wheelPosition.current.move > -30 && flag.current) {
+      flag.current = false;
+
+      return;
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", function () {
+      wheelPosition.current.scroll = this.scrollY;
+    });
+  });
+
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }, [date]);
+
+  useEffect(() => {
+    if (isInitialRender.current) {
+      isInitialRender.current = false;
+
+      return;
+    }
+
+    refetch();
+  }, [date]);
+
   return (
-    <Styled.Container>
-      <PreviousInfiniteScroll
-        callback={fetchPreviousPage}
+    <Styled.Container onWheel={onWheel}>
+      <SearchInput placeholder="검색 할 키워드를 입력해주세요." />
+      <NextInfiniteScroll
+        callback={fetchNextPage}
         threshold={0.9}
-        endPoint={!hasPreviousPage}
+        endPoint={!hasNextPage}
       >
-        <NextInfiniteScroll
-          callback={fetchNextPage}
-          threshold={0.9}
-          endPoint={!hasNextPage}
-        >
-          <FlexColumn gap="4px" width="100%">
-            {isPreviousDataLoading && (
-              <>
-                {Array.from({ length: 20 }).map((_, index) => (
-                  <MessageCardSkeleton key={index} />
-                ))}
-              </>
-            )}
+        <FlexColumn gap="4px" width="100%">
+          {isFetching && (
+            <>
+              {Array.from({ length: 20 }).map((_, index) => (
+                <MessageCardSkeleton key={index} />
+              ))}
+            </>
+          )}
 
-            {extractMessages(previousData, nextData).map(
-              ({ id, username, postedDate, text, userThumbnail }) => (
-                <React.Fragment key={id}>
-                  <>
-                    {renderDateDropdown(postedDate)}
-                    <MessageCard
-                      username={username}
-                      date={postedDate}
-                      text={text}
-                      thumbnail={userThumbnail}
-                    />
-                  </>
-                </React.Fragment>
-              )
-            )}
+          {data?.pages
+            .flatMap((arr) => arr.messages)
+            .map(({ id, username, postedDate, text, userThumbnail }) => (
+              <React.Fragment key={id}>
+                {renderDateDropdown(postedDate)}
+                <MessageCard
+                  username={username}
+                  date={postedDate}
+                  text={text}
+                  thumbnail={userThumbnail}
+                />
+              </React.Fragment>
+            ))}
 
-            {isNextDataLoading && (
-              <>
-                {Array.from({ length: 20 }).map((_, index) => (
-                  <MessageCardSkeleton key={index} />
-                ))}
-              </>
-            )}
-          </FlexColumn>
-        </NextInfiniteScroll>
-      </PreviousInfiniteScroll>
+          {isFetching && (
+            <>
+              {Array.from({ length: 20 }).map((_, index) => (
+                <MessageCardSkeleton key={index} />
+              ))}
+            </>
+          )}
+        </FlexColumn>
+      </NextInfiniteScroll>
     </Styled.Container>
   );
 }
