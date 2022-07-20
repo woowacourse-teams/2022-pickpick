509b27fe352c443868b5c9ab4540a911eff11344
bomi <bonita9931@gmail.com>
2022-07-20 13:36:23 +0900

    fix: ChannelOrderRequest 기본 생성자 추가

--- backend/src/main/java/com/pickpick/controller/dto/ChannelOrderRequest.java
+++ backend/src/main/java/com/pickpick/controller/dto/ChannelOrderRequest.java
@@ -8,6 +8,9 @@ public class ChannelOrderRequest {
     private Long id;
     private int order;
 
+    public ChannelOrderRequest() {
+    }
+
     public ChannelOrderRequest(Long id, int order) {
         this.id = id;
         this.order = order;
