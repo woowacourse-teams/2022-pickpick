5ab642aa06dbe253ad341af59ce492ebe75cf485
hyewoncc <hyewoncc@gmail.com>
2022-07-20 11:49:45 +0900

    feat: 예외 발생 시 로깅
    Co-authored-by: hyewoncc <hyewoncc@gmail.com>
    Co-authored-by: JangBomi <bonita9931@gmail.com>

--- backend/src/main/java/com/pickpick/controller/ControllerAdvice.java
+++ backend/src/main/java/com/pickpick/controller/ControllerAdvice.java
@@ -2,11 +2,13 @@ package com.pickpick.controller;
 
 import com.pickpick.exception.SubscriptionDuplicatedException;
 import com.pickpick.exception.SubscriptionNotExistException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class ControllerAdvice {
 
@@ -15,6 +17,7 @@ public class ControllerAdvice {
             SubscriptionDuplicatedException.class,
             SubscriptionNotExistException.class
     })
-    public void handleBadRequest() {
+    public void handleBadRequest(final RuntimeException e) {
+        log.error("예외 발생 : " + e.getMessage());
     }
 }
