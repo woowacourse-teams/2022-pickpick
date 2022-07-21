e3bc740dc0f49479c04f3cb78856e8f29d7af874
yeon-06 <rnjstldus2@gmail.com>
2022-07-21 16:19:22 +0900

    style: 띄어쓰기 컨벤션 준수

--- backend/src/main/java/com/pickpick/exception/BadRequestException.java
+++ backend/src/main/java/com/pickpick/exception/BadRequestException.java
@@ -1,6 +1,6 @@
 package com.pickpick.exception;
 
-public class BadRequestException extends RuntimeException{
+public class BadRequestException extends RuntimeException {
 
     public BadRequestException(final String message) {
         super(message);
--- backend/src/main/java/com/pickpick/exception/NotFoundException.java
+++ backend/src/main/java/com/pickpick/exception/NotFoundException.java
@@ -1,6 +1,6 @@
 package com.pickpick.exception;
 
-public class NotFoundException extends RuntimeException{
+public class NotFoundException extends RuntimeException {
 
     public NotFoundException(final String message) {
         super(message);
