61bd3d26ee75e7797e98d84cb4e56fed8c76cc01
moonheekim0118 <hellomooneekim@gmail.com>
2022-07-22 10:01:17 +0900

    fix: api 임시 토큰 수정

--- frontend/src/api/channels.ts
+++ frontend/src/api/channels.ts
@@ -4,7 +4,7 @@ import { fetcher } from ".";
 export const getChannels = async () => {
   const { data } = await fetcher.get<ResponseChannels>("/api/channels", {
     headers: {
-      authorization: "Bearer 1",
+      authorization: "Bearer 47",
     },
   });
   return data;
@@ -16,7 +16,7 @@ export const subscribeChannel = async (id: string) => {
     { id },
     {
       headers: {
-        authorization: "Bearer 1",
+        authorization: "Bearer 47",
       },
     }
   );
@@ -25,7 +25,7 @@ export const subscribeChannel = async (id: string) => {
 export const unsubscribeChannel = async (id: string) => {
   await fetcher.delete(`/api/channel-subscription/${id}`, {
     headers: {
-      authorization: "Bearer 1",
+      authorization: "Bearer 47",
     },
   });
 };
