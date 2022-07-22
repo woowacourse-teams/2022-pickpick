ab4d05865c7993773ff14ed57da9f14387092502
bomi <bonita9931@gmail.com>
2022-07-22 13:36:52 +0900

    refactor: ChannelSubscriptionControllerTest로부터 ChannelControllerTest 분리

---
+++ backend/src/test/java/com/pickpick/controller/ChannelControllerTest.java
@@ -0,0 +1,32 @@
+package com.pickpick.controller;
+
+import org.apache.http.HttpHeaders;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@Sql({"/truncate.sql", "/channel.sql"})
+class ChannelControllerTest extends RestDocsTestSupport {
+
+    @DisplayName("구독 여부를 포함하여 채널을 조회한다.")
+    @Test
+    void findAll() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/api/channels")
+                        .header(HttpHeaders.AUTHORIZATION, "Bearer 2"))
+                .andExpect(status().isOk())
+                .andDo(restDocs.document(
+                        responseFields(
+                                fieldWithPath("channels.[].id").type(JsonFieldType.NUMBER).description("채널 아이디"),
+                                fieldWithPath("channels.[].name").type(JsonFieldType.STRING).description("채널 이름"),
+                                fieldWithPath("channels.[].isSubscribed").type(JsonFieldType.BOOLEAN).description("채널 구독 여부")
+                        )
+                ));
+    }
+}
--- backend/src/test/java/com/pickpick/controller/ChannelSubscriptionControllerTest.java
+++ backend/src/test/java/com/pickpick/controller/ChannelSubscriptionControllerTest.java
@@ -19,22 +19,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @Sql({"/truncate.sql", "/channel.sql"})
 class ChannelSubscriptionControllerTest extends RestDocsTestSupport {
 
-    @DisplayName("구독 여부를 포함하여 채널을 조회한다.")
-    @Test
-    void findAll() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders
-                        .get("/api/channels")
-                        .header(HttpHeaders.AUTHORIZATION, "Bearer 2"))
-                .andExpect(status().isOk())
-                .andDo(restDocs.document(
-                        responseFields(
-                                fieldWithPath("channels.[].id").type(JsonFieldType.NUMBER).description("채널 아이디"),
-                                fieldWithPath("channels.[].name").type(JsonFieldType.STRING).description("채널 이름"),
-                                fieldWithPath("channels.[].isSubscribed").type(JsonFieldType.BOOLEAN).description("채널 구독 여부")
-                        )
-                ));
-    }
-
     @DisplayName("멤버의 구독 중인 채널을 조회한다.")
     @Test
     void findAllSubscribedChannel() throws Exception {
