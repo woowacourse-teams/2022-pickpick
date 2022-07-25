f7407768fb61be5de7ac5492f7d9d60d04c25b1d
hj-rich <ztzy1907@gmail.com>
2022-07-26 02:14:09 +0900

    refactor: 검색어로 메시지 조회시 대소문자 무시하도록 개선, 테스트 픽스처 수정

--- backend/src/test/java/com/pickpick/acceptance/MessageAcceptanceTest.java
+++ backend/src/test/java/com/pickpick/acceptance/MessageAcceptanceTest.java
@@ -68,13 +68,13 @@ class MessageAcceptanceTest extends AcceptanceTest {
                         false,
                         createExpectedMessageIds(26L, 7L)),
                 Arguments.of(
-                        "channelIds가 5이고, keyword가 A일 경우, 5번 채널의 메시지 중 A가 포함된 메시지 20개를 시간 내림차순으로 응답해야 한다.",
-                        createQueryParams("A", "", "5", "", "", ""),
+                        "channelIds가 5이고, keyword가 text A일 경우, 5번 채널의 메시지 중 text A가 포함된 메시지 20개를 시간 내림차순으로 응답해야 한다.",
+                        createQueryParams("text A", "", "5", "", "", ""),
                         false,
                         createExpectedMessageIds(32L, 13L)),
                 Arguments.of(
-                        "channelIds가 5이고, keyword가 A이고, needPastMessage가 true이고 messageId가 존재할 경우, 5번 채널의 A가 포함된 메시지 중, 전달된 메시지 ID의 메시지보다 더 과거 메시지 20개를 시간 내림차순으로 응답해야 한다.",
-                        createQueryParams("A", "", "5", "", "13", ""),
+                        "channelIds가 5이고, keyword가 text A이고, needPastMessage가 true이고 messageId가 존재할 경우, 5번 채널의 text A가 포함된 메시지 중, 전달된 메시지 ID의 메시지보다 더 과거 메시지 20개를 시간 내림차순으로 응답해야 한다.",
+                        createQueryParams("text A", "", "5", "", "13", ""),
                         true,
                         createExpectedMessageIds(12L, 8L))
         );
