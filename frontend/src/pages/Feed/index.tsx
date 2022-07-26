bd2fe6d53bf6ef679374fafb62d580a18d042f5a
moonheekim0118 <hellomooneekim@gmail.com>
2022-07-26 16:00:14 +0900

    refactor: Feed Page 리팩토링 적용 - useMessageDate 적용 - infinte query callback 함수 적용 - MessagesLoadingStatus 적용 - extractResponseMessages 분리 및 적용
    Co-authored-by: Jaejeung Ko <kkojae91@users.noreply.github.com>

--- frontend/src/pages/Feed/index.tsx
+++ frontend/src/pages/Feed/index.tsx
@@ -1,51 +1,28 @@
 import { FlexColumn } from "@src/@styles/shared";
-import Dropdown from "@src/components/Dropdown";
 import MessageCard from "@src/components/MessageCard";
 import SearchInput from "@src/components/SearchInput";
-import MessageCardSkeleton from "@src/components/MessageCardSkeleton";
 import * as Styled from "./style";
-import { InfiniteData, useInfiniteQuery } from "react-query";
+import { useInfiniteQuery } from "react-query";
 import { getMessages } from "@src/api/messages";
-import { Message, ResponseMessages } from "@src/@types/shared";
-import React, { useRef } from "react";
+import { ResponseMessages } from "@src/@types/shared";
+import React from "react";
 import InfiniteScroll from "@src/components/@shared/InfiniteScroll";
+import MessagesLoadingStatus from "@src/components/MessagesLoadingStatus";
+import { extractResponseMessages } from "@src/@utils";
+import useMessageDate from "@src/hooks/useMessageDate";
+import DateDropDown from "@src/components/DateDropdown";
+import { nextMessagesCallback } from "@src/api/utils";
 
 function Feed() {
+  const { initializeDateArray, isRenderDate } = useMessageDate();
+
   const { data, isLoading, isError, fetchNextPage, hasNextPage } =
     useInfiniteQuery<ResponseMessages>(["messages"], getMessages(), {
-      getNextPageParam: ({ isLast, messages }) => {
-        if (!isLast) {
-          return { messageId: messages[messages.length - 1]?.id };
-        }
-      },
-      onSettled: () => {
-        dateArrayRef.current = [];
-      },
+      getNextPageParam: nextMessagesCallback,
+      onSettled: initializeDateArray,
     });
 
-  const dateArrayRef = useRef<string[]>([]);
-
   if (isError) return <div>이거슨 에러양!!!!</div>;
-
-  const extractMessages = (
-    data?: InfiniteData<ResponseMessages>
-  ): Message[] => {
-    if (!data) return [];
-
-    return data.pages.flatMap((arr) => arr.messages);
-  };
-
-  const renderDateDropdown = (postedDate: string) => {
-    if (dateArrayRef.current.includes(postedDate)) return;
-
-    dateArrayRef.current.push(postedDate);
-
-    return (
-      <Styled.Wrapper>
-        <Dropdown postedDate={postedDate} />
-      </Styled.Wrapper>
-    );
-  };
 
   return (
     <Styled.Container>
@@ -57,27 +34,27 @@ function Feed() {
         endPoint={!hasNextPage}
       >
         <FlexColumn gap="4px" width="100%">
-          {extractMessages(data).map(
-            ({ id, username, postedDate, text, userThumbnail }) => (
-              <React.Fragment key={id}>
-                {renderDateDropdown(postedDate.split("T")[0])}
-                <MessageCard
-                  username={username}
-                  date={postedDate}
-                  text={text}
-                  thumbnail={userThumbnail}
-                />
-              </React.Fragment>
-            )
+          {extractResponseMessages(data).map(
+            ({ id, username, postedDate, text, userThumbnail }) => {
+              const parsedDate = postedDate.split("T")[0];
+
+              return (
+                <React.Fragment key={id}>
+                  {isRenderDate(parsedDate) && (
+                    <DateDropDown postedDate={parsedDate} />
+                  )}
+                  <MessageCard
+                    username={username}
+                    date={postedDate}
+                    text={text}
+                    thumbnail={userThumbnail}
+                  />
+                </React.Fragment>
+              );
+            }
           )}
 
-          {isLoading && (
-            <>
-              {Array.from({ length: 20 }).map((_, index) => (
-                <MessageCardSkeleton key={index} />
-              ))}
-            </>
-          )}
+          {isLoading && <MessagesLoadingStatus length={20} />}
         </FlexColumn>
       </InfiniteScroll>
     </Styled.Container>
