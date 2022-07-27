ed91cbca37118cc7cb4c0ae2dca5fa652d91b967
kkojae <jaejeung3210@gmail.com>
2022-07-27 15:36:07 +0900

    chore: 개행 추가
    Co-authored-by: moonheekim0118 <hellomooneekim@gmail.com>

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,6 +5,7 @@ import { LIGHT_MODE_THEME } from "@src/@styles/theme";
 import GlobalStyle from "./@styles/GlobalStyle";
 import { QueryClientProvider, QueryClient } from "react-query";
 import { BrowserRouter } from "react-router-dom";
+
 if (process.env.NODE_ENV === "development") {
   // eslint-disable-next-line @typescript-eslint/no-var-requires
   const { worker } = require("./mocks/browser");
