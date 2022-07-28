8a1151f17c64ad7243ea2fa1fc4e087092978eff
bomi <bonita9931@gmail.com>
2022-07-28 17:45:13 +0900

    feat: Slack Event 요청값 로깅
    Co-authored-by: JangBomi <bonita9931@gmail.com>
    Co-authored-by: Richard Jeon <ztzy1907@gmail.com>

--- backend/src/main/java/com/pickpick/slackevent/ui/SlackEventController.java
+++ backend/src/main/java/com/pickpick/slackevent/ui/SlackEventController.java
@@ -3,12 +3,14 @@ package com.pickpick.slackevent.ui;
 import com.pickpick.slackevent.application.SlackEvent;
 import com.pickpick.slackevent.application.SlackEventServiceFinder;
 import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/event")
 public class SlackEventController {
@@ -26,6 +28,8 @@ public class SlackEventController {
 
     @PostMapping
     public ResponseEntity<String> save(@RequestBody Map<String, Object> requestBody) {
+        log.info("Slack Event: {}", requestBody);
+
         if (isUrlVerificationRequest(requestBody)) {
             return ResponseEntity.ok((String) requestBody.get(CHALLENGE));
         }
