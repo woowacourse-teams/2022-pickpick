d90303eab8fecbe4b2ee8f8e17835f08bf1cc7b0
kkojae <jaejeung3210@gmail.com>
2022-07-29 13:30:42 +0900

    feat: LazyLoading component 개발

---
+++ frontend/src/components/LazyLoading/index.tsx
@@ -0,0 +1,12 @@
+import * as Styled from "./style";
+
+function LazyLoading() {
+  return (
+    <Styled.Container>
+      <Styled.LeftCircle />
+      <Styled.RightCircle />
+    </Styled.Container>
+  );
+}
+
+export default LazyLoading;
