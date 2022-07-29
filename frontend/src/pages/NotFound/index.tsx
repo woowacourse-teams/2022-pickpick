4f0ce6b2fe1e67afafeb53c459bcf2a082168ee2
Jaejeung Ko <jaejeung3210@gmail.com>
2022-07-30 00:15:39 +0900

    style: NotFound component style 추가 (#235)

--- frontend/src/pages/NotFound/index.tsx
+++ frontend/src/pages/NotFound/index.tsx
@@ -1,5 +1,21 @@
+import { PATH_NAME } from "@src/@constants";
+import { FlexColumn } from "@src/@styles/shared";
+import Button from "@src/components/@shared/Button";
+import { Link } from "react-router-dom";
+import * as Styled from "./style";
+
 function NotFound() {
-  return <div>NotFound 페이지입니다.</div>;
+  return (
+    <FlexColumn gap="30px" margin="30vh 0" alignItems="center">
+      <Styled.Title>잘못된 경로 입니다.</Styled.Title>
+      <Styled.Description>
+        피드로 돌아가시려면 아래 버튼을 클릭해주세요.
+      </Styled.Description>
+      <Link to={PATH_NAME.FEED}>
+        <Button>피드로 가기</Button>
+      </Link>
+    </FlexColumn>
+  );
 }
 
 export default NotFound;
