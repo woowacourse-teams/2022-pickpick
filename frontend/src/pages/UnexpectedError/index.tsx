1b36e5b55b108a5084f5036add27b2d303082293
kkojae <jaejeung3210@gmail.com>
2022-07-29 16:15:35 +0900

    style: Unexpected Error component style 추가

--- frontend/src/pages/UnexpectedError/index.tsx
+++ frontend/src/pages/UnexpectedError/index.tsx
@@ -1,17 +1,21 @@
 import { PATH_NAME } from "@src/@constants";
 import Footer from "@src/components/@layouts/Footer";
 import Header from "@src/components/@layouts/Header";
+import Button from "@src/components/@shared/Button";
 import * as Styled from "@src/components/@layouts/LayoutContainer/style";
+import { FlexColumn } from "@src/@styles/shared";
 
 function UnexpectedError() {
   return (
     <Styled.Container>
       <Header />
       <Styled.Main hasMarginTop={true}>
-        <h1>예상치 못한 에러 발생</h1>
-        <a href={PATH_NAME.FEED}>
-          <button>메인 피드로 가기</button>
-        </a>
+        <FlexColumn gap={"30px"} margin={"30vh 0"}>
+          <h1>예상치 못한 에러 발생</h1>
+          <a href={PATH_NAME.FEED}>
+            <Button>메인 피드로 가기</Button>
+          </a>
+        </FlexColumn>
       </Styled.Main>
       <Footer />
     </Styled.Container>
