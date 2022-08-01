6b773b96d120a55bb5dfa07d1f7f7002f39bb04c
moonheekim0118 <hellomooneekim@gmail.com>
2022-08-01 22:53:50 +0900

    feat: Private Router 컴포넌트 구현

---
+++ frontend/src/components/PrivateRouter/index.tsx
@@ -0,0 +1,27 @@
+import { PATH_NAME, QUERY_KEY } from "@src/@constants";
+import { isAuthenticated } from "@src/api/auth";
+import { useEffect } from "react";
+import { useQuery } from "react-query";
+import { useNavigate } from "react-router-dom";
+
+interface Props {
+  children: JSX.Element;
+}
+
+function PrivateRouter({ children }: Props) {
+  const navigate = useNavigate();
+
+  const { isError } = useQuery([QUERY_KEY.AUTHENTICATION], isAuthenticated, {
+    suspense: true,
+  });
+
+  useEffect(() => {
+    if (isError) {
+      navigate(PATH_NAME.HOME);
+    }
+  }, [isError]);
+
+  return <>{children}</>;
+}
+
+export default PrivateRouter;
