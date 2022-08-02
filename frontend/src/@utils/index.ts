d59aa44f09edc127104086aa0fd2df80fd792000
kkojae <jaejeung3210@gmail.com>
2022-08-02 10:36:50 +0900

    refactor: utils 파일 상수처리

--- frontend/src/@utils/index.ts
+++ frontend/src/@utils/index.ts
@@ -6,11 +6,24 @@ import {
 } from "@src/@types/shared";
 import { InfiniteData } from "react-query";
 
-const getTimeStandard = (time: number): string => {
-  if (time < 12) return `오전 ${time}`;
-  if (time === 12) return `오후 ${12}`;
+const CONVERTER_SUFFIX = "T23:59:59";
 
-  return `오후 ${time - 12}`;
+const DATE = {
+  TODAY: "오늘",
+  YESTERDAY: "어제",
+} as const;
+
+const TIME = {
+  AM: "오전",
+  PM: "오후",
+  NOON: 12,
+} as const;
+
+const getTimeStandard = (time: number): string => {
+  if (time < TIME.NOON) return `${TIME.AM} ${time}`;
+  if (time === TIME.NOON) return `${TIME.PM} ${TIME.NOON}`;
+
+  return `${TIME.PM} ${time - TIME.NOON}`;
 };
 
 export const parseTime = (date: string): string => {
@@ -38,18 +51,23 @@ export const extractResponseBookmarks = (
   return data.pages.flatMap((arr) => arr.bookmarks);
 };
 
-export const ISOConverter = (date: string) => {
+export const ISOConverter = (date: string): string => {
   const today = new Date();
 
-  if (date === "오늘") {
-    return today.toISOString().split("T")[0] + "T23:59:59";
+  if (date === DATE.TODAY) {
+    return `${today.toISOString().split("T")[0]}${CONVERTER_SUFFIX}`;
   }
 
-  if (date === "어제") {
+  if (date === DATE.YESTERDAY) {
     const yesterday = new Date(today.setDate(today.getDate() - 1));
-    return yesterday.toISOString().split("T")[0] + "T23:59:59";
+
+    return `${yesterday.toISOString().split("T")[0]}${CONVERTER_SUFFIX}`;
   }
 
   const [year, month, day] = date.split("-");
-  return `${year}-${month.padStart(2, "0")}-${day.padStart(2, "0")}T23:59:59`;
+
+  return `${year}-${month.padStart(2, "0")}-${day.padStart(
+    2,
+    "0"
+  )}${CONVERTER_SUFFIX}`;
 };
