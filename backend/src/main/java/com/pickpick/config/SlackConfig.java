4bac848b24e116f15486254aa6ea94af1e033c61
hyewoncc <80666066+hyewoncc@users.noreply.github.com>
2022-08-02 10:55:22 +0900

    refactor: MemberInitializer에 빈 주입 및 테스트에서 비활성화   (#252)
    * refactor: MemberInitializer에 빈 주입 및 Profile 설정
    
    * refactor: 슬랙 api 호출을 위한 봇 토큰을 빈 생성 시 설정
    
    * refactor: 메서드 명 개선

--- backend/src/main/java/com/pickpick/config/SlackConfig.java
+++ backend/src/main/java/com/pickpick/config/SlackConfig.java
@@ -2,14 +2,18 @@ package com.pickpick.config;
 
 import com.slack.api.Slack;
 import com.slack.api.methods.MethodsClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SlackConfig {
 
+    @Value("${slack.bot-token}")
+    private String token;
+
     @Bean
     public MethodsClient methodsClient() {
-        return Slack.getInstance().methods();
+        return Slack.getInstance().methods(token);
     }
 }
