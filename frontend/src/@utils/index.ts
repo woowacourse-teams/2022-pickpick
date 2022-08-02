e48b09837ff9ac8b9e0486cf8a3f98456ce6e5c6
moonheekim0118 <hellomooneekim@gmail.com>
2022-08-02 21:51:00 +0900

    refactor: 정규식 주석 첨부

--- frontend/src/@utils/index.ts
+++ frontend/src/@utils/index.ts
@@ -52,7 +52,7 @@ export const setCookie = (key: string, value: string) => {
 };
 
 export const getCookie = (key: string) => {
-  const regex = new RegExp(`(?<=${key}=)[^;]*`);
+  const regex = new RegExp(`(?<=${key}=)[^;]*`); // key(좌항)에 해당하는 우항을 가져온다. 세미콜론은 제외한다.
   const matches = document.cookie.match(regex);
   return matches ? matches[0] : undefined;
 };
