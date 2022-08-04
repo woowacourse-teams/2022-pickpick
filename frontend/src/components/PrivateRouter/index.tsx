66e73eeaa61bd74ab63e809c0072b8cc56771040
moonheekim0118 <hellomooneekim@gmail.com>
2022-08-04 17:19:36 +0900

    hotfix: 로그인 후 쿼리 무효화 추가 (#296)

--- frontend/src/components/PrivateRouter/index.tsx
+++ frontend/src/components/PrivateRouter/index.tsx
@@ -19,7 +19,7 @@ function PrivateRouter({ children }: Props) {
     isCertificated,
     {
       useErrorBoundary: false,
-      retry: 3,
+      retry: false,
     }
   );
 
--- frontend/src/hooks/useAuthentication.ts
+++ frontend/src/hooks/useAuthentication.ts
@@ -1,14 +1,21 @@
-import { ACCESS_TOKEN_KEY, QUERY_KEY, MESSAGES } from "@src/@constants";
+import {
+  ACCESS_TOKEN_KEY,
+  QUERY_KEY,
+  MESSAGES,
+  PATH_NAME,
+} from "@src/@constants";
 import { deleteCookie, setCookie } from "@src/@utils";
 import { useQueryClient } from "react-query";
+import { useNavigate } from "react-router-dom";
 import useSnackbar from "./useSnackbar";
 
 function useAuthentication() {
   const queryClient = useQueryClient();
+  const navigate = useNavigate();
   const { openSuccessSnackbar } = useSnackbar();
 
-  const login = () => {
-    setCookie(ACCESS_TOKEN_KEY, "test");
+  const login = (token: string) => {
+    setCookie(ACCESS_TOKEN_KEY, token);
     queryClient.invalidateQueries(QUERY_KEY.AUTHENTICATION);
     openSuccessSnackbar(MESSAGES.LOGIN_SUCCESS);
   };
@@ -16,6 +23,7 @@ function useAuthentication() {
   const logout = () => {
     deleteCookie(ACCESS_TOKEN_KEY);
     queryClient.invalidateQueries(QUERY_KEY.AUTHENTICATION);
+    navigate(PATH_NAME.HOME);
   };
 
   return { login, logout };
