8e957c81cde27ccf08a221af4056e69154833831
yeon-06 <rnjstldus2@gmail.com>
2022-08-04 16:18:44 +0900

    test: 테스트 케이스 추가

--- backend/src/test/java/com/pickpick/channel/domain/ChannelSubscriptionTest.java
+++ backend/src/test/java/com/pickpick/channel/domain/ChannelSubscriptionTest.java
@@ -1,14 +1,29 @@
 package com.pickpick.channel.domain;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import com.pickpick.exception.SubscriptionOrderMinException;
 import com.pickpick.member.domain.Member;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class ChannelSubscriptionTest {
+
+    @DisplayName("채널 구독 순서 변경")
+    @Test
+    void changeSubscriptionOrder() {
+        // given
+        int order = 0;
+        Channel channel = new Channel("slackId", "채널 이름");
+        Member member = new Member("slackId", "유저 이름", "Profile.png");
+        ChannelSubscription channelSubscription = new ChannelSubscription(channel, member, 1);
+
+        // when & then
+        assertDoesNotThrow(() -> channelSubscription.changeOrder(order));
+    }
 
     @DisplayName("채널 구독 순서는 1 미만일 수 없다.")
     @ValueSource(ints = {0, -1})
