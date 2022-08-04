062677f68f3f7364b4c496e9113121a25f0c01d4
moonheekim0118 <hellomooneekim@gmail.com>
2022-08-04 15:36:47 +0900

    feat: Mocking API url 수정

--- frontend/src/mocks/handlers/auth.ts
+++ frontend/src/mocks/handlers/auth.ts
@@ -1,7 +1,7 @@
 import { rest } from "msw";
 
 const handlers = [
-  rest.get("/api/auth", (req, res, ctx) => {
+  rest.get("/api/certification", (req, res, ctx) => {
     return res(ctx.status(200), ctx.delay(500));
   }),
 ];
