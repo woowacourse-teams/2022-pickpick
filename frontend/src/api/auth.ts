2c7fdad2e625d5ad177b9681c2e7786631c6a82e
moonheekim0118 <hellomooneekim@gmail.com>
2022-08-04 16:06:23 +0900

    fix: 쿠키에 토큰 저장하는 로직 api 함수로 이동

--- frontend/src/api/auth.ts
+++ frontend/src/api/auth.ts
@@ -1,6 +1,7 @@
-import { API_ENDPOINT } from "@src/@constants";
+import { ACCESS_TOKEN_KEY, API_ENDPOINT } from "@src/@constants";
 import { privateFetcher, publicFetcher } from ".";
 import { ResponseToken } from "@src/@types/shared";
+import { setCookie } from "@src/@utils";
 
 export const isCertificated = async () => {
   const { data } = await privateFetcher.get(API_ENDPOINT.CERTIFICATION);
@@ -11,5 +12,6 @@ export const slackLogin = async (code: string) => {
   const { data } = await publicFetcher.get<ResponseToken>(
     `${API_ENDPOINT.SLACK_LOGIN}?code=${code}`
   );
+  setCookie(ACCESS_TOKEN_KEY, data?.token ?? "");
   return data;
 };
