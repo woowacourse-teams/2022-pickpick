777a7b02c433261c5ab20b7cb621a2048d71ecf9
moonheekim0118 <hellomooneekim@gmail.com>
2022-08-08 17:17:02 +0900

    feat: EmptyStatus 컴포넌트 작성

---
+++ frontend/src/components/EmptyStatus/index.stories.js
@@ -0,0 +1,10 @@
+import EmptyStatus from ".";
+
+export default {
+  title: "Component/EmptyStatus",
+  component: EmptyStatus,
+};
+
+const Template = (args) => <EmptyStatus {...args} />;
+
+export const DefaultTemplate = Template.bind({});
