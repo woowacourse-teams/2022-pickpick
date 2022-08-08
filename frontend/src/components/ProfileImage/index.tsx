4f4ecccb41e15ccf41cbbc1ad67998fc58c87432
moonheekim0118 <hellomooneekim@gmail.com>
2022-08-08 18:40:21 +0900

    feat: ProfileImage 없을 경우 디폴트 이미지 보여주도록 구현

--- frontend/src/components/ProfileImage/index.tsx
+++ frontend/src/components/ProfileImage/index.tsx
@@ -1,8 +1,13 @@
 import React from "react";
+import DefaultProfileImage from "@public/assets/images/DefaultProfileImage.png";
 import * as Styled from "./style";
 
-function ProfileImage(props: React.ImgHTMLAttributes<HTMLImageElement>) {
-  return <Styled.Container {...props} />;
+function ProfileImage({
+  // eslint-disable-next-line react/prop-types
+  src,
+  ...props
+}: React.ImgHTMLAttributes<HTMLImageElement>) {
+  return <Styled.Container src={src || DefaultProfileImage} {...props} />;
 }
 
 export default ProfileImage;
