ab0d22ecd5c0ad139eba70d64a09cdc4c3830bfd
kkojae <jaejeung3210@gmail.com>
2022-08-09 22:53:01 +0900

    feat: SearchForm component form, input tag eventHandler 작성

--- frontend/src/components/SearchForm/index.tsx
+++ frontend/src/components/SearchForm/index.tsx
@@ -4,13 +4,19 @@ import SearchInput from "@src/components/SearchInput";
 import * as Styled from "./style";
 import useSelectChannels from "@src/hooks/useSelectChannels";
 import useModal from "@src/hooks/useModal";
-import { FormEvent } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { PATH_NAME } from "@src/@constants";
+import useSnackbar from "@src/hooks/useSnackbar";
 
 interface Props {
   channelId: number;
 }
 
 function SearchForm({ channelId }: Props) {
+  const navigate = useNavigate();
+  const [searchKeyword, setSearchKeyword] = useState("");
+
   const {
     channelsData,
     channelIds,
@@ -27,9 +33,32 @@ function SearchForm({ channelId }: Props) {
     handleCloseModal: handleCloseSearchOptions,
   } = useModal();
 
+  const { openFailureSnackbar } = useSnackbar();
+
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
-    console.log("submit");
+
+    if (!channelIds.length) {
+      openFailureSnackbar("채널을 하나 이상 선택 후 검색 버튼을 눌러주세요.");
+
+      return;
+    }
+
+    if (!searchKeyword.length) {
+      openFailureSnackbar(
+        "검색할 키워드를 입력하신 후 검색 버튼을 눌러주세요."
+      );
+
+      return;
+    }
+
+    navigate(
+      `${PATH_NAME.SEARCH_RESULT}?keyword=${searchKeyword}&channelIds=${channelIds}`
+    );
+  };
+
+  const handleChangeSearchKeyword = (event: ChangeEvent<HTMLInputElement>) => {
+    setSearchKeyword(event.target.value);
   };
 
   return (
@@ -41,6 +70,8 @@ function SearchForm({ channelId }: Props) {
         <SearchInput
           placeholder="검색 할 키워드를 입력해주세요."
           onFocus={handleOpenSearchOptions}
+          onChange={handleChangeSearchKeyword}
+          value={searchKeyword}
         >
           {channelsData && defaultChannel && isSearchInputFocused && (
             <SearchOptions
