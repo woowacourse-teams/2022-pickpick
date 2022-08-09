32d55d6b1b3d1bf3db454990425de032c9f116c0
kkojae <jaejeung3210@gmail.com>
2022-08-09 15:37:44 +0900

    feat: useChannelIds custom hook 개발

---
+++ frontend/src/hooks/useChannelIds.ts
@@ -0,0 +1,79 @@
+import { QUERY_KEY } from "@src/@constants";
+import { getSubscribedChannels } from "@src/api/channels";
+import { useEffect, useState } from "react";
+import { useQuery } from "react-query";
+import useModal from "./useModal";
+
+function useChannelIds(channelId: number | undefined) {
+  const [channelIds, setChannelIds] = useState<(number | undefined)[]>([]);
+
+  const { data: channelsData } = useQuery(
+    QUERY_KEY.SUBSCRIBED_CHANNELS,
+    getSubscribedChannels
+  );
+
+  const {
+    isModalOpened: isSearchInputFocused,
+    handleOpenModal: handleOpenSearchOptions,
+    handleCloseModal: handleCloseSearchOptions,
+  } = useModal();
+
+  const defaultChannel = channelsData?.channels.filter((channel) => {
+    if (channelId === 0) {
+      return channel.id === channelsData?.channels[0].id;
+    }
+
+    return channel.id === channelId;
+  })[0];
+
+  const handleToggleAllChannelIds = () => {
+    if (!channelsData) return;
+
+    if (
+      0 <= channelIds.length &&
+      channelIds.length < channelsData.channels.length
+    ) {
+      setChannelIds(channelsData.channels.map((channel) => channel.id));
+
+      return;
+    }
+
+    setChannelIds([]);
+    return;
+  };
+
+  const handleToggleChannelId = (id: number) => {
+    if (channelIds.includes(id)) {
+      setChannelIds((prev) => {
+        prev.splice(
+          prev.findIndex((channelId) => channelId === id),
+          1
+        );
+
+        return [...prev];
+      });
+
+      return;
+    }
+
+    setChannelIds((prev) => [...prev, id]);
+    return;
+  };
+
+  useEffect(() => {
+    setChannelIds([channelId === 0 ? channelsData?.channels[0].id : channelId]);
+  }, [channelId]);
+
+  return {
+    isSearchInputFocused,
+    channelsData,
+    channelIds,
+    defaultChannel,
+    handleOpenSearchOptions,
+    handleCloseSearchOptions,
+    handleToggleChannelId,
+    handleToggleAllChannelIds,
+  };
+}
+
+export default useChannelIds;
