2d4cae28185a4f7a602f8012e9b352624d22d7e2
moonheekim0118 <hellomooneekim@gmail.com>
2022-08-09 11:43:38 +0900

    refactor: 불필요한 콜백함수 제거

--- frontend/src/components/EmptyStatus/index.tsx
+++ frontend/src/components/EmptyStatus/index.tsx
@@ -7,7 +7,7 @@ function EmptyStatus() {
   return (
     <FlexColumn gap="30px" margin="25vh 0" alignItems="center">
       <h3>조회된 결과가 없습니다.</h3>
-      <Button onClick={() => pushPreviousPage()}>뒤로가기</Button>
+      <Button onClick={pushPreviousPage}>뒤로가기</Button>
     </FlexColumn>
   );
 }
