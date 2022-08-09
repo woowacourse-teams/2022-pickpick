221576b9d4881bdd51a778a0a29ccbfc1f1a2bba
hyewoncc <hyewoncc@gmail.com>
2022-08-09 17:28:53 +0900

    refactor: 예외 클래스 생성 파라미터 명이 외래키일 경우 연관관계 도메인명을 붙여 구체적 표기

--- backend/src/main/java/com/pickpick/exception/channel/SubscriptionDuplicateException.java
+++ backend/src/main/java/com/pickpick/exception/channel/SubscriptionDuplicateException.java
@@ -7,8 +7,8 @@ public class SubscriptionDuplicateException extends BadRequestException {
     private static final String DEFAULT_MESSAGE = "구독 중인 채널 중복 구독 시도";
     private static final String CLIENT_MESSAGE = "이미 구독 중인 채널입니다.";
 
-    public SubscriptionDuplicateException(final Long id) {
-        super(String.format("%s -> subscription channel id: %d", DEFAULT_MESSAGE, id));
+    public SubscriptionDuplicateException(final Long channelId) {
+        super(String.format("%s -> subscription channel id: %d", DEFAULT_MESSAGE, channelId));
     }
 
     @Override
--- backend/src/main/java/com/pickpick/exception/channel/SubscriptionNotFoundException.java
+++ backend/src/main/java/com/pickpick/exception/channel/SubscriptionNotFoundException.java
@@ -8,7 +8,7 @@ public class SubscriptionNotFoundException extends NotFoundException {
     private static final String CLIENT_MESSAGE = "해당 멤버가 구독 중인 채널이 없습니다.";
 
     public SubscriptionNotFoundException(final Long memberId) {
-        super(String.format("%s -> member id: %d", DEFAULT_MESSAGE, memberId));
+        super(String.format("%s -> subscription member id: %d", DEFAULT_MESSAGE, memberId));
     }
 
     @Override
