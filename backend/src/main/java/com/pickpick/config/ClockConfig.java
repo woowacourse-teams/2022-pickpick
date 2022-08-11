21f1f906fff4c730f422514ca16cb94d7bdcd6b8
bomi <bonita9931@gmail.com>
2022-08-11 20:09:40 +0900

    refactor: LocalDateTime.now()에서 Clock을 파라미터로 받도록 수정
    Co-authored-by: JangBomi <bonita9931@gmail.com>
    Co-authored-by: yeon-06 <yeonlog06@gmail.com>

---
+++ backend/src/main/java/com/pickpick/config/ClockConfig.java
@@ -0,0 +1,14 @@
+package com.pickpick.config;
+
+import java.time.Clock;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ClockConfig {
+
+    @Bean
+    public Clock clock() {
+        return Clock.systemDefaultZone();
+    }
+}
