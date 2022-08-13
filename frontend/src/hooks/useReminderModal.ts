5e66b5ea32acb24b7e44f043a3b523a8f9293c0b
kkojae <jaejeung3210@gmail.com>
2022-08-13 16:44:20 +0900

    feat: useReminderModal custom hook 개발

---
+++ frontend/src/hooks/useReminderModal.ts
@@ -0,0 +1,211 @@
+import { getDateInformation, getMeridiemTime } from "@src/@utils";
+import { ChangeEvent, useEffect, useRef, useState } from "react";
+
+const convertTimeToStepTenMinuteTime = ({
+  hour,
+  minute,
+}: {
+  hour: number;
+  minute: number;
+}) => {
+  if (minute > 50) {
+    return { parsedHour: hour + 1, parsedMinute: 0 };
+  }
+
+  return { parsedHour: hour, parsedMinute: Math.ceil(minute / 10) * 10 };
+};
+
+const convertMeridiemHourToStandardHour = (
+  meridiem: string,
+  meridiemHour: number
+): number => {
+  if (meridiem === "오후") {
+    if (meridiemHour === 12) {
+      return 0;
+    }
+
+    return meridiemHour + 12;
+  }
+
+  return meridiemHour;
+};
+
+function useReminderModal() {
+  const { year, month, date, hour, minute } = getDateInformation(new Date());
+  const { meridiem, hour: meridiemHour } = getMeridiemTime(hour);
+  const lastDate = new Date(year, month, 0).getDate();
+  const { parsedHour, parsedMinute } = convertTimeToStepTenMinuteTime({
+    hour: meridiemHour,
+    minute,
+  });
+
+  const yearRef = useRef<HTMLDivElement>(null);
+  const monthRef = useRef<HTMLDivElement>(null);
+  const dateRef = useRef<HTMLDivElement>(null);
+
+  const meridiemRef = useRef<HTMLDivElement>(null);
+  const hourRef = useRef<HTMLDivElement>(null);
+  const minuteRef = useRef<HTMLDivElement>(null);
+
+  const [isDropdownOpened, setIsDropdownOpened] = useState(false);
+
+  const [checkedMeridiem, setCheckedMeridiem] = useState(meridiem);
+  const [checkedHour, setCheckedHour] = useState(parsedHour.toString());
+  const [checkedMinute, setCheckedMinute] = useState(parsedMinute.toString());
+
+  const [checkedYear, setCheckedYear] = useState(year.toString());
+  const [checkedMonth, setCheckedMonth] = useState(month.toString());
+  const [checkedDate, setCheckedDate] = useState(date.toString());
+
+  const meridiems = ["오전", "오후"];
+  const hours = Array.from({ length: 12 }, (_, index) =>
+    (index + 1).toString()
+  );
+  const minutes = Array.from({ length: 6 }, (_, index) =>
+    (index * 10).toString()
+  );
+  const years = [year, year + 1, year + 2].map((year) => year.toString());
+  const months = Array.from({ length: 12 }, (_, index) =>
+    (index + 1).toString()
+  );
+  const dates = Array.from({ length: lastDate }, (_, index) =>
+    (index + 1).toString()
+  );
+
+  const handleChangeMeridiem = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target.value === "오전" || event.target.value === "오후") {
+      setCheckedMeridiem(event.target.value);
+    }
+  };
+
+  const handleChangeHour = (event: ChangeEvent<HTMLInputElement>) => {
+    setCheckedHour(event.target.value);
+  };
+
+  const handleChangeMinute = (event: ChangeEvent<HTMLInputElement>) => {
+    setCheckedMinute(event.target.value);
+  };
+
+  const handleChangeYear = (event: ChangeEvent<HTMLInputElement>) => {
+    setCheckedYear(event.target.value);
+  };
+
+  const handleChangeMonth = (event: ChangeEvent<HTMLInputElement>) => {
+    setCheckedMonth(event.target.value);
+  };
+
+  const handleChangeDate = (event: ChangeEvent<HTMLInputElement>) => {
+    setCheckedDate(event.target.value);
+  };
+
+  const handleOpenDropdown = (isOpened: boolean) => {
+    setIsDropdownOpened(isOpened);
+  };
+
+  const handleSubmit = () => {
+    if (
+      Number(checkedYear) < year ||
+      Number(checkedMonth) < month ||
+      Number(checkedDate) < date ||
+      convertMeridiemHourToStandardHour(checkedMeridiem, Number(checkedHour)) <
+        hour ||
+      Number(checkedMinute) < minute
+    ) {
+      console.log("리마인더 시간은 현재 시간 이후로 설정해주셔야 합니다.");
+      return;
+    }
+
+    console.log("서브밋 성공");
+  };
+
+  useEffect(() => {
+    if (yearRef.current) {
+      yearRef.current.scrollTo({
+        top: (Number(checkedYear) - year) * 22,
+        behavior: "smooth",
+      });
+    }
+
+    if (monthRef.current) {
+      monthRef.current.scrollTo({
+        top: (Number(checkedMonth) - 1) * 22.5,
+        behavior: "smooth",
+      });
+    }
+
+    if (dateRef.current) {
+      dateRef.current.scrollTo({
+        top: (Number(checkedDate) - 1) * 22.5,
+        behavior: "smooth",
+      });
+    }
+
+    if (meridiemRef.current) {
+      meridiemRef.current.scrollTo({
+        top: checkedMeridiem === "오전" ? 0 : 22,
+        behavior: "smooth",
+      });
+    }
+
+    if (hourRef.current) {
+      hourRef.current.scrollTo({
+        top: (Number(checkedHour) - 1) * 22.5,
+        behavior: "smooth",
+      });
+    }
+
+    if (minuteRef.current) {
+      minuteRef.current.scrollTo({
+        top: (Number(checkedMinute) / 10) * 22.7,
+        behavior: "smooth",
+      });
+    }
+  }, [
+    checkedMeridiem,
+    checkedHour,
+    checkedMinute,
+    checkedYear,
+    checkedMonth,
+    checkedDate,
+    isDropdownOpened,
+  ]);
+
+  return {
+    ref: {
+      yearRef,
+      monthRef,
+      dateRef,
+      meridiemRef,
+      hourRef,
+      minuteRef,
+    },
+    checkedState: {
+      checkedMeridiem,
+      checkedHour,
+      checkedMinute,
+      checkedYear,
+      checkedMonth,
+      checkedDate,
+    },
+    dateStateArray: {
+      meridiems,
+      hours,
+      minutes,
+      years,
+      months,
+      dates,
+    },
+    handler: {
+      handleChangeMeridiem,
+      handleChangeHour,
+      handleChangeMinute,
+      handleChangeYear,
+      handleChangeMonth,
+      handleChangeDate,
+      handleOpenDropdown,
+      handleSubmit,
+    },
+  };
+}
+
+export default useReminderModal;
