a1f95168a248775cbc2d03b7b0b2e3788892a558
kkojae <jaejeung3210@gmail.com>
2022-08-14 17:22:03 +0900

    refactor: ReminderModalToggleDropdown component border color theme 적용

--- frontend/src/components/ReminderModalToggleDropdown/style.ts
+++ frontend/src/components/ReminderModalToggleDropdown/style.ts
@@ -5,11 +5,14 @@ export const Container = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  border: 1px solid #dcdcdc;
   border-radius: 4px;
   padding: 2px 6px;
   cursor: pointer;
   z-index: 1;
+
+  ${({ theme }: StyledDefaultProps) => css`
+    border: 1px solid ${theme.COLOR.BACKGROUND.TERTIARY};
+  `}
 `;
 
 export const Text = styled.p`
