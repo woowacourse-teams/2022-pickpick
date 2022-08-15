62556b8f205fa4cfd037e05504e70b31eaef1195
moonheekim0118 <hellomooneekim@gmail.com>
2022-08-15 16:06:37 +0900

    refactor: 작업한 파일 내 누락된 절대경로 수정

--- frontend/src/components/Drawer/index.tsx
+++ frontend/src/components/Drawer/index.tsx
@@ -1,12 +1,12 @@
 import * as Styled from "./style";
 import PlusIcon from "@public/assets/icons/PlusIcon.svg";
 import { FlexColumn, FlexRow } from "@src/@styles/shared";
-import WrapperLink from "../@shared/WrapperLink";
+import WrapperLink from "@src/components/@shared/WrapperLink";
 import { PATH_NAME } from "@src/@constants";
 import { SubscribedChannel, Theme } from "@src/@types/shared";
 import { Link } from "react-router-dom";
 import { useTheme } from "styled-components";
-import ThemeToggler from "../ThemeToggler";
+import ThemeToggler from "@src/components/ThemeToggler";
 
 interface Props {
   channels?: SubscribedChannel[];
