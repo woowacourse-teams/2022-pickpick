d5c85172cd8a5e74ab8949917f69689b44960dbe
moonheekim0118 <hellomooneekim@gmail.com>
2022-08-17 01:06:10 +0900

    fix: 선택된 채널이 데이터 매 리렌더링마다 비워지는 문제 해결

--- frontend/src/hooks/useSelectChannels.ts
+++ frontend/src/hooks/useSelectChannels.ts
@@ -50,7 +50,7 @@ function useSelectChannels({ currentChannelIds }: Props): ReturnType {
   };
 
   useEffect(() => {
-    if (allChannels.length === 0) return;
+    if (allChannels.length === 0 || selectedChannelIds.length > 0) return;
     setSelectedChannelIds(
       currentChannelIds.length === 0 ? [allChannels[0].id] : currentChannelIds
     );
