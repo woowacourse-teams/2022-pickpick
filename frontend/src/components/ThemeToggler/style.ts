540ad5dbafd2dfd54846b9851ded7096d7f7072f
moonheekim0118 <hellomooneekim@gmail.com>
2022-08-18 21:49:40 +0900

    refactor: QA 4차 반영  (#476)
    * refactor: 불필요한 주석 제거
    
    * fix: 다크모드 토글러 icon assets 에 추가 및 사용
    
    * fix: OS 환경에 따라서 초기 다크모드/라이트모드 설정되도록 수정
    
    * refactor: 서버에서 변경된 북마크 스키마 반영
    
    * fix: 개발모드 환경변수 파일 수정
    
    * fix: 기본채널 (feed) 일 경우에도 drawer 에 하이라이트 표시되도록 수정

--- frontend/src/components/ThemeToggler/style.ts
+++ frontend/src/components/ThemeToggler/style.ts
@@ -1,41 +1,52 @@
 import { StyledDefaultProps } from "@src/@types/shared";
 import styled, { css } from "styled-components";
 
-export const Container = styled.input`
-    appearance: none;
-    width: 62px;
-    height: 32px;
-    display: inline-block;
-    position: relative;
-    border-radius: 50px;
-    overflow: hidden;
-    outline: none;
-    border: none;
-    cursor: pointer;
-    ${({ theme }: StyledDefaultProps) => css`
-      background-color: ${theme.COLOR.TEXT.DEFAULT};
-    `}
-    &::before { 
-      content: url('https://shivanarrthine.com/public/images/icons/moon.svg');
-      display: block;
-      position: absolute;
-    
-      width: 24px;
-      height: 24px;
+export const Container = styled.label`
+  width: 62px;
+  height: 32px;
+  position: relative;
+`;
 
-      left: 4px;
-      top: 4px;
-      border-radius: 50%;
-      text-indent: 4px;
-      line-height: 29px;
-      word-spacing: 37px;
-      white-space: nowrap;
-      transition: all cubic-bezier(0.3, 1.5, 0.7, 1) 0.3s;
-    }
-  &:checked{
-    &::before{
-        left: 32px;
-        content: url('https://shivanarrthine.com/public/images/icons/sun.svg');
-      }|
-  }
+export const CheckBox = styled.input`
+  appearance: none;
+  width: 62px;
+  height: 32px;
+  display: inline-block;
+  position: relative;
+  border-radius: 50px;
+  overflow: hidden;
+  outline: none;
+  border: none;
+  cursor: pointer;
+  ${({ theme }: StyledDefaultProps) => css`
+    background-color: ${theme.COLOR.TEXT.DEFAULT};
+  `}
+`;
+
+interface IconContainerProps extends StyledDefaultProps {
+  isVisible: boolean;
+}
+
+export const LeftIconContainer = styled.div`
+  transform: scaleY(1);
+  position: absolute;
+  top: -12px;
+  left: 10px;
+  cursor: pointer;
+  transition: opacity cubic-bezier(0.3, 1.5, 0.7, 1) 0.5s;
+  ${({ isVisible }: IconContainerProps) => css`
+    opacity: ${isVisible ? "1" : "0"};
+  `}
+`;
+
+export const RightIconContainer = styled.div`
+  transform: scaleY(1);
+  position: absolute;
+  top: -12px;
+  right: 10px;
+  cursor: pointer;
+  transition: all cubic-bezier(0.3, 1.5, 0.7, 1) 0.5s;
+  ${({ isVisible }: IconContainerProps) => css`
+    opacity: ${isVisible ? "1" : "0"};
+  `}
 `;
