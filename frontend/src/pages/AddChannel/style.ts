fbf9ac1f9afabe0f8678b27f65fcd4ad28ed59b7
Jaejeung Ko <jaejeung3210@gmail.com>
2022-08-18 22:27:11 +0900

    refactor: 채널선택 페이지에서 네비게이션 바 제거 및 디자인 수정 (#477)

--- frontend/src/pages/AddChannel/style.ts
+++ frontend/src/pages/AddChannel/style.ts
@@ -1,10 +1,25 @@
-import styled from "styled-components";
+import { StyledDefaultProps } from "@src/@types/shared";
+import styled, { css } from "styled-components";
 
 export const Container = styled.div`
   padding: 20px;
   display: flex;
   flex-direction: column;
   min-width: 320px;
+`;
+
+export const Title = styled.h1`
+  margin-bottom: 10px;
+  ${({ theme }: StyledDefaultProps) => css`
+    font-family: ${theme.FONT.SECONDARY};
+    font-size: ${theme.FONT_SIZE.SUBTITLE};
+  `}
+`;
+
+export const Description = styled.p`
+  ${({ theme }: StyledDefaultProps) => css`
+    font-size: ${theme.FONT_SIZE.LARGE_BODY};
+  `}
 `;
 
 export const ChannelListContainer = styled.div`
@@ -15,5 +30,20 @@ export const ChannelListContainer = styled.div`
   width: 100%;
   min-width: 320px;
   gap: 10px;
-  margin-top: 20px;
+  margin: 40px 0;
+`;
+
+export const Button = styled.button`
+  display: block;
+  border-radius: 50px;
+  white-space: nowrap;
+  cursor: pointer;
+  background-color: inherit;
+  padding: 0.25rem 0.75rem;
+  font-weight: 600;
+
+  ${({ theme }: StyledDefaultProps) => css`
+    font-size: ${theme.FONT_SIZE.LARGE_BODY};
+    border: 1px solid ${theme.COLOR.PRIMARY.DEFAULT};
+  `}
 `;
