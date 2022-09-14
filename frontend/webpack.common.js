48178a5de8a58186d1f6d7fe8dba5b7fa33f94a7
kkojae <jaejeung3210@gmail.com>
2022-09-14 17:03:47 +0900

    chore: 파일 확장자 설정 (웹팩, d.ts)
    Co-authored-by: moonheekim0118 <hellomooneekim@gmail.com>

--- frontend/webpack.common.js
+++ frontend/webpack.common.js
@@ -26,7 +26,7 @@ module.exports = {
         exclude: /node_modules/,
       },
       {
-        test: /\.(png|jpe?g|woff)$/i,
+        test: /\.(png|jpe?g|woff|woff2|ttf)$/i,
         type: "asset/resource",
       },
       {
