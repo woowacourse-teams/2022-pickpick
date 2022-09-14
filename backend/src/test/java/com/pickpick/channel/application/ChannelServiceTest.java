52c79e4715a93058b375f28c0892802a15a8e424
yeon-06 <rnjstldus2@gmail.com>
2022-09-14 15:39:11 +0900

    test: 채널 전체 조회 테스트 추가

--- backend/src/test/java/com/pickpick/channel/application/ChannelServiceTest.java
+++ backend/src/test/java/com/pickpick/channel/application/ChannelServiceTest.java
@@ -8,7 +8,11 @@ import static org.mockito.BDDMockito.given;
 
 import com.pickpick.channel.domain.Channel;
 import com.pickpick.channel.domain.ChannelRepository;
+import com.pickpick.channel.ui.dto.ChannelResponse;
+import com.pickpick.channel.ui.dto.ChannelResponses;
 import com.pickpick.exception.SlackApiCallException;
+import com.pickpick.member.domain.Member;
+import com.pickpick.member.domain.MemberRepository;
 import com.slack.api.RequestConfigurator;
 import com.slack.api.methods.MethodsClient;
 import com.slack.api.methods.SlackApiException;
@@ -16,7 +20,9 @@ import com.slack.api.methods.request.conversations.ConversationsInfoRequest.Conv
 import com.slack.api.methods.response.conversations.ConversationsInfoResponse;
 import com.slack.api.model.Conversation;
 import java.io.IOException;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,6 +42,9 @@ class ChannelServiceTest {
 
     @Autowired
     private ChannelService channelService;
+
+    @Autowired
+    private MemberRepository members;
 
     @DisplayName("채널을 저장한다.")
     @Test
@@ -78,5 +87,32 @@ class ChannelServiceTest {
         conversationsInfoResponse.setChannel(conversation);
 
         return conversationsInfoResponse;
+    }
+
+    @DisplayName("전체 채널 목록을 조회한다.")
+    @Test
+    void selectAllChannels() {
+        // given
+        Member member = saveMember();
+        Channel channel1 = saveChannel("slackId1", "공지사항");
+        Channel channel2 = saveChannel("slackId2", "잡담 게시판");
+
+        // when
+        ChannelResponses responses = channelService.findAll(member.getId());
+
+        // then
+        List<Long> actualIds = responses.getChannels()
+                .stream()
+                .map(ChannelResponse::getId)
+                .collect(Collectors.toList());
+        assertThat(actualIds).containsExactly(channel1.getId(), channel2.getId());
+    }
+
+    private Member saveMember() {
+        return members.save(new Member("TESTMEMBER", "테스트 계정", "test.png"));
+    }
+
+    private Channel saveChannel(final String slackId, final String channelName) {
+        return channels.save(new Channel(slackId, channelName));
     }
 }
