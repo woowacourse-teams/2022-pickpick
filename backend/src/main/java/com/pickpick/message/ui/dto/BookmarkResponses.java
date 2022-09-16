adcd29321f20958d62f0183166fca67303734d72
hyewoncc <hyewoncc@gmail.com>
2022-09-16 17:12:30 +0900

    refactor: 북마크의 isLast 필드를 hasPast로 변경

--- backend/src/main/java/com/pickpick/message/ui/dto/BookmarkResponses.java
+++ backend/src/main/java/com/pickpick/message/ui/dto/BookmarkResponses.java
@@ -9,14 +9,14 @@ public class BookmarkResponses {
 
     private List<BookmarkResponse> bookmarks;
 
-    @JsonProperty(value = "isLast")
-    private boolean last;
+    @JsonProperty(value = "hasPast")
+    private boolean hasPast;
 
     private BookmarkResponses() {
     }
 
-    public BookmarkResponses(final List<BookmarkResponse> bookmarks, final boolean last) {
+    public BookmarkResponses(final List<BookmarkResponse> bookmarks, final boolean hasPast) {
         this.bookmarks = bookmarks;
-        this.last = last;
+        this.hasPast = hasPast;
     }
 }
