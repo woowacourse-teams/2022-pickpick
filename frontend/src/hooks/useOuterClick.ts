f0455cb21837ab1e3472310eb4bffcd49bc942f2
moonheekim0118 <hellomooneekim@gmail.com>
2022-09-22 14:23:06 +0900

    fix: Drawer 내부 클릭 시, Drawer 가 닫히는 문제 해결 (#529)
    * refactor: useOuterClick 훅 수정
    - props 로 내려준 숫자에 따라서 innerRef 를 n개 생성 할 수 있도록 수정
    
    * fix: Drawer 내부 클릭 시, Drawer 닫히는 문제 해결
    - 변경된 useOuterClick 훅 적용하여 여러군데에 innerRef 적용 할 수 있도록 수정
    
    * refactor: useOuterClick 훅 적용
    
    * refactor: useOuterClick 에서 props 로 받은 ref 숫자가 0일 경우, 길이가 1개인 배열을 디폴트로 생성하도록 수정
    
    * fix: useOuterClick의 이벤트 리스너에서 불필요한 조건문 제거
    - Array 의 length 가 무조건 1개 이상이기 때문에, 불필요한 가드로 판단되어 제거
    
    * fix: nullish 연산자 or 연산자로 수정
    
    * refactor: useOuterClick 함수에서 innerRef 배열 크기 지정을 강제하도록 수정
    - requiredRefCount -> requiredInnerRefCount 로 네이밍 수정
    - requiredInnerRefCount 타입 옵셔널 제거
    - requiredInnerRefCount 가 0보다 같거나 작을 경우 1로 강제하도록 수정
    
    * refactor: useOuterClick 반환 값 주석 작성
    
    * refactor: useOuterClick 반환값 수정
    - 기존에 객체형태로 반환하던 것을 array 로 수정

--- frontend/src/hooks/useOuterClick.ts
+++ frontend/src/hooks/useOuterClick.ts
@@ -2,18 +2,26 @@ import { useEffect, useRef } from "react";
 
 type CallbackType = () => void;
 
-function useOuterClick(callback: CallbackType) {
-  const callbackRef = useRef<CallbackType>();
-  const innerRef = useRef<HTMLDivElement>(null);
+interface Props {
+  callback: CallbackType;
+  requiredInnerRefCount: number;
+}
 
+function useOuterClick({ callback, requiredInnerRefCount = 1 }: Props) {
+  const callbackRef = useRef<CallbackType>();
+  const innerRefArray = [
+    ...Array(requiredInnerRefCount <= 0 ? 1 : requiredInnerRefCount),
+  ].map(() => useRef<HTMLDivElement>(null));
   callbackRef.current = callback;
 
   useEffect(() => {
     const handleClick = (event: MouseEvent) => {
       if (
-        innerRef.current &&
         callbackRef.current &&
-        !innerRef.current.contains(event.target as HTMLDivElement)
+        !innerRefArray.some(
+          (ref) =>
+            ref.current && ref.current.contains(event.target as HTMLDivElement)
+        )
       ) {
         callbackRef.current();
       }
@@ -22,9 +30,9 @@ function useOuterClick(callback: CallbackType) {
     document.addEventListener("click", handleClick);
 
     return () => document.removeEventListener("click", handleClick);
-  }, [callbackRef, innerRef]);
+  }, []);
 
-  return { innerRef };
+  return innerRefArray;
 }
 
 export default useOuterClick;
