8d457c69835126a08ed6bdb262dde5c22ccf9c8a
bomi <bonita9931@gmail.com>
2022-09-29 18:16:56 +0900

    refactor: ChannelControllerTest 개선

--- backend/src/test/java/com/pickpick/channel/ui/ChannelControllerTest.java
+++ backend/src/test/java/com/pickpick/channel/ui/ChannelControllerTest.java
@@ -1,53 +1,66 @@
 package com.pickpick.channel.ui;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
 import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
 import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.pickpick.auth.support.JwtTokenProvider;
+import com.pickpick.channel.ui.dto.ChannelResponse;
+import com.pickpick.channel.ui.dto.ChannelResponses;
 import com.pickpick.config.RestDocsTestSupport;
+import java.util.List;
 import org.apache.http.HttpHeaders;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import org.springframework.restdocs.payload.JsonFieldType;
-import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.ResultActions;
 
-@Sql({"/channel.sql", "/channel-subscription.sql"})
 class ChannelControllerTest extends RestDocsTestSupport {
-
-    @MockBean
-    private JwtTokenProvider jwtTokenProvider;
-
-    @BeforeEach
-    void setup() {
-        given(jwtTokenProvider.getPayload(any(String.class)))
-                .willReturn("2");
-    }
 
     @DisplayName("구독 여부를 포함하여 채널을 조회한다.")
     @Test
     void findAll() throws Exception {
-        mockMvc.perform(RestDocumentationRequestBuilders
+        ChannelResponses channels = createChannelResponses();
+        when(channelService.findAll(anyLong()))
+                .thenReturn(channels);
+
+        ResultActions result = mockMvc.perform(RestDocumentationRequestBuilders
                         .get("/api/channels")
-                        .header(HttpHeaders.AUTHORIZATION, "Bearer 2"))
-                .andExpect(status().isOk())
-                .andDo(restDocs.document(
-                        requestHeaders(
-                                headerWithName(HttpHeaders.AUTHORIZATION).description("유저 식별 토큰(Bearer)")
-                        ),
-                        responseFields(
-                                fieldWithPath("channels.[].id").type(JsonFieldType.NUMBER).description("채널 아이디"),
-                                fieldWithPath("channels.[].name").type(JsonFieldType.STRING).description("채널 이름"),
-                                fieldWithPath("channels.[].isSubscribed").type(JsonFieldType.BOOLEAN)
-                                        .description("채널 구독 여부")
-                        )
-                ));
+                        .header(HttpHeaders.AUTHORIZATION, "Bearer provided.jwt.token"))
+                .andExpect(status().isOk());
+
+        //docs
+        result.andDo(restDocs.document(
+                requestHeaders(
+                        headerWithName(HttpHeaders.AUTHORIZATION).description("유저 식별 토큰(Bearer)")
+                ),
+                responseFields(
+                        fieldWithPath("channels.[].id").type(JsonFieldType.NUMBER).description("채널 아이디"),
+                        fieldWithPath("channels.[].name").type(JsonFieldType.STRING).description("채널 이름"),
+                        fieldWithPath("channels.[].isSubscribed").type(JsonFieldType.BOOLEAN)
+                                .description("채널 구독 여부")
+                )
+        ));
+    }
+
+    private ChannelResponses createChannelResponses() {
+        return new ChannelResponses(
+                List.of(
+                        ChannelResponse.builder()
+                                .id(1L)
+                                .name("공지사항")
+                                .subscribed(false)
+                                .build(),
+                        ChannelResponse.builder()
+                                .id(2L)
+                                .name("잡담")
+                                .subscribed(true)
+                                .build()
+                )
+        );
     }
 }
--- backend/src/test/java/com/pickpick/config/RestDocsTestSupport.java
+++ backend/src/test/java/com/pickpick/config/RestDocsTestSupport.java
@@ -1,12 +1,29 @@
 package com.pickpick.config;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.AfterEach;
+import com.pickpick.auth.application.AuthService;
+import com.pickpick.auth.support.JwtTokenProvider;
+import com.pickpick.auth.ui.AuthController;
+import com.pickpick.channel.application.ChannelService;
+import com.pickpick.channel.application.ChannelSubscriptionService;
+import com.pickpick.channel.ui.ChannelController;
+import com.pickpick.channel.ui.ChannelSubscriptionController;
+import com.pickpick.message.application.BookmarkService;
+import com.pickpick.message.application.MessageService;
+import com.pickpick.message.application.ReminderService;
+import com.pickpick.message.ui.BookmarkController;
+import com.pickpick.message.ui.MessageController;
+import com.pickpick.message.ui.ReminderController;
+import com.pickpick.slackevent.application.SlackEventServiceFinder;
+import com.pickpick.slackevent.ui.SlackEventController;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
@@ -17,11 +34,44 @@ import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureMockMvc
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@AutoConfigureMockMvc
+@WebMvcTest({
+        AuthController.class,
+        ChannelController.class,
+        ChannelSubscriptionController.class,
+        BookmarkController.class,
+        MessageController.class,
+        ReminderController.class,
+        SlackEventController.class
+})
 @ExtendWith(RestDocumentationExtension.class)
 @Import(RestDocsConfiguration.class)
 public class RestDocsTestSupport {
+
+    @MockBean
+    protected AuthService authService;
+
+    @MockBean
+    protected ChannelService channelService;
+
+    @MockBean
+    protected ChannelSubscriptionService channelSubscriptionService;
+
+    @MockBean
+    protected BookmarkService bookmarkService;
+
+    @MockBean
+    protected MessageService messageService;
+
+    @MockBean
+    protected ReminderService reminderService;
+
+    @MockBean
+    protected JwtTokenProvider jwtTokenProvider;
+
+    @MockBean
+    protected SlackEventServiceFinder slackEventServiceFinder;
 
     @Autowired
     protected MockMvc mockMvc;
@@ -32,8 +82,8 @@ public class RestDocsTestSupport {
     @Autowired
     protected ObjectMapper objectMapper;
 
-    @Autowired
-    private DatabaseCleaner databaseCleaner;
+//    @Autowired
+//    private DatabaseCleaner databaseCleaner;
 
     @BeforeEach
     public void setUp(
@@ -47,8 +97,14 @@ public class RestDocsTestSupport {
                 .build();
     }
 
-    @AfterEach
-    void clear() {
-        databaseCleaner.clear();
+    @BeforeEach
+    void setup() {
+        given(jwtTokenProvider.getPayload(any()))
+                .willReturn("1"); // memberId를 반환
     }
+
+//    @AfterEach
+//    void clear() {
+//        databaseCleaner.clear();
+//    }
 }
