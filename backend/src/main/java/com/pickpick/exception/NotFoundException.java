36ae24ac0332f3f621dca7415a55dc14e9bc6604
bomi <bonita9931@gmail.com>
2022-10-01 16:22:53 +0900

    refactor: notFoundException 의 error code와 client message 필드 관리로 수정

--- backend/src/main/java/com/pickpick/exception/NotFoundException.java
+++ backend/src/main/java/com/pickpick/exception/NotFoundException.java
@@ -2,18 +2,20 @@ package com.pickpick.exception;
 
 public class NotFoundException extends RuntimeException {
 
-    private static final String ERROR_CODE = "NOT_FOUND";
-    private static final String CLIENT_MESSAGE = "해당 정보를 조회하지 못했습니다.";
+    private String errorCode = "NOT_FOUND";
+    private String clientMessage = "해당 정보를 조회하지 못했습니다.";
 
     public NotFoundException(final String serverMessage, final String clientMessage, final String errorCode) {
         super(serverMessage);
+        this.clientMessage = clientMessage;
+        this.errorCode = errorCode;
     }
 
     public String getErrorCode() {
-        return ERROR_CODE;
+        return errorCode;
     }
 
     public String getClientMessage() {
-        return CLIENT_MESSAGE;
+        return clientMessage;
     }
 }
