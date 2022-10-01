d38202043bee8f5c4d40c7752fcacf94a8ba64a0
bomi <bonita9931@gmail.com>
2022-10-01 16:27:03 +0900

    refactor: 불필요한 세미콜론 제거

--- backend/src/main/java/com/pickpick/exception/slackevent/SlackEventServiceNotFoundException.java
+++ backend/src/main/java/com/pickpick/exception/slackevent/SlackEventServiceNotFoundException.java
@@ -7,7 +7,6 @@ public class SlackEventServiceNotFoundException extends NotFoundException {
 
     private static final String ERROR_CODE = "SLACK_EVENT_SERVICE_NOT_FOUND";
     private static final String SERVER_MESSAGE = "대응하는 Service 클래스가 없는 Slack Event 요청";
-    ;
     private static final String CLIENT_MESSAGE = "지원하지 않는 SlackEvent입니다.";
 
     public SlackEventServiceNotFoundException(final SlackEvent slackEvent) {
