68ff26243dc93968b675859bbcda33d5bb28a124
bomi <bonita9931@gmail.com>
2022-10-04 17:43:42 +0900

    테스트를 위한 임시 커밋

--- backend/src/test/java/com/pickpick/support/DocsControllerTest.java
+++ backend/src/test/java/com/pickpick/support/DocsControllerTest.java
@@ -20,7 +20,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
-import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
@@ -37,7 +36,6 @@ import org.springframework.web.context.WebApplicationContext;
 @WebMvcTest
 @ExtendWith(RestDocumentationExtension.class)
 @Import(RestDocsConfiguration.class)
-@MockBean(JpaMetamodelMappingContext.class)
 public class DocsControllerTest {
 
     private static final String BEARER_JWT_TOKEN = "Bearer provided.jwt.token";
