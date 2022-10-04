fa4a3b0c9eabbfd3d38eea1a8084c79dbcddd81f
bomi <bonita9931@gmail.com>
2022-10-04 17:16:03 +0900

    fix: Auditing 관련 MockBean 추가하여 테스트 실패 해결

--- backend/src/test/java/com/pickpick/support/DocsControllerTest.java
+++ backend/src/test/java/com/pickpick/support/DocsControllerTest.java
@@ -20,6 +20,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
@@ -36,6 +37,7 @@ import org.springframework.web.context.WebApplicationContext;
 @WebMvcTest
 @ExtendWith(RestDocumentationExtension.class)
 @Import(RestDocsConfiguration.class)
+@MockBean(JpaMetamodelMappingContext.class)
 public class DocsControllerTest {
 
     private static final String BEARER_JWT_TOKEN = "Bearer provided.jwt.token";
