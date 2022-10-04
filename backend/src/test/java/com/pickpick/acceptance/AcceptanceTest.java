c0d6ec49b6dc2a59a6b92d5e7c5cab9f34a06ab7
yeon-06 <rnjstldus2@gmail.com>
2022-10-04 22:25:50 +0900

    test: 불필요한 모킹 객체 제거

--- backend/src/test/java/com/pickpick/acceptance/AcceptanceTest.java
+++ backend/src/test/java/com/pickpick/acceptance/AcceptanceTest.java
@@ -5,14 +5,12 @@ import com.pickpick.support.DatabaseCleaner;
 import com.pickpick.support.ExternalClient;
 import com.pickpick.support.TestConfig;
 import io.restassured.RestAssured;
-import java.time.Clock;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.annotation.Import;
 
@@ -23,9 +21,6 @@ public class AcceptanceTest {
 
     @LocalServerPort
     int port;
-
-    @SpyBean
-    protected Clock clock;
 
     @Autowired
     protected ExternalClient slackClient;
