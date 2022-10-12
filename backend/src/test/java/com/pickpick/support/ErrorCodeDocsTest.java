c00fbeafdda8070de02dae5251833e89e914a034
yeon-06 <rnjstldus2@gmail.com>
2022-10-12 14:23:05 +0900

    test: 사용하지 않는 결과값 제거

--- backend/src/test/java/com/pickpick/support/ErrorCodeDocsTest.java
+++ backend/src/test/java/com/pickpick/support/ErrorCodeDocsTest.java
@@ -4,7 +4,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 class ErrorCodeDocsTest extends DocsControllerTestBase {
@@ -14,7 +13,7 @@ class ErrorCodeDocsTest extends DocsControllerTestBase {
     void errorCodes() throws Exception {
         ErrorCodeSnippet errorCodeSnippet = new ErrorCodeSnippet("error-code", "error-code-template");
 
-        ResultActions result = mockMvc.perform(MockMvcRequestBuilders
+        mockMvc.perform(MockMvcRequestBuilders
                         .get("/test/error-code"))
                 .andExpect(status().isOk())
                 .andDo(restDocs.document(errorCodeSnippet));
