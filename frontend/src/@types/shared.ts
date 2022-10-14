261f7017e95ebf7753972e03bae2668be97edcac
moonheekim0118 <hellomooneekim@gmail.com>
2022-10-14 19:45:03 +0900

    fix: Theme type 정의 및 타입 선언 적용 - 기존에 제대로 정의하지 않은 Theme type 정의 - Theme 관련 상수에 해당 Theme type 선언

--- frontend/src/@types/shared.ts
+++ frontend/src/@types/shared.ts
@@ -1,8 +1,52 @@
 import { SNACKBAR_STATUS, THEME_KIND } from "@src/@constants";
 import { API_ERROR_MESSAGE } from "@src/@constants/message";
-import { LIGHT_MODE_THEME } from "@src/@styles/theme";
 
-export type Theme = typeof LIGHT_MODE_THEME;
+export type Theme = {
+  COLOR: {
+    PRIMARY: { DEFAULT: string };
+    SECONDARY: { DEFAULT: string };
+    TEXT: {
+      DEFAULT: string;
+      DISABLED: string;
+      PLACEHOLDER: string;
+      LIGHT_BLUE: string;
+      WHITE: string;
+    };
+    BACKGROUND: {
+      PRIMARY: string;
+      SECONDARY: string;
+      TERTIARY: string;
+    };
+    WRAPPER: {
+      DEFAULT: string;
+      DISABLED: string;
+    };
+    CONTAINER: {
+      DEFAULT: string;
+      WHITE: string;
+      LIGHT_RED: string;
+      LIGHT_BLUE: string;
+      LIGHT_ORANGE: string;
+      GRADIENT_ORANGE: string;
+    };
+    BORDER: string;
+    DIMMER: string;
+    STAR_ICON_FILL: string;
+  };
+  FONT: {
+    PRIMARY: "Roboto";
+    SECONDARY: "Twayair";
+  };
+  FONT_SIZE: {
+    TITLE: string;
+    SUBTITLE: string;
+    X_LARGE_BODY: string;
+    LARGE_BODY: string;
+    BODY: string;
+    PLACEHOLDER: string;
+    CAPTION: string;
+  };
+};
 
 export type ThemeKind = keyof typeof THEME_KIND;
 
