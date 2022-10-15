9176f123c0dc4c6527a6ff75b6184dd499f019fe
moonheekim0118 <hellomooneekim@gmail.com>
2022-10-16 01:40:10 +0900

    refactor: 누락되어 하드코딩된 color 를 theme color 로 적용

--- frontend/src/components/Calendar/index.tsx
+++ frontend/src/components/Calendar/index.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { useTheme } from "styled-components";
 
 import WrapperButton from "@src/components/@shared/WrapperButton";
 import ArrowIconLeft from "@src/components/@svgIcons/ArrowIconLeft";
@@ -26,11 +27,17 @@ function Calendar({ channelId, handleCloseCalendar }: Props) {
     handleIncrementMonth,
   } = useCalendar();
 
+  const theme = useTheme();
+
   return (
     <Styled.Container>
       <Styled.Month>
         <WrapperButton kind="smallIcon" onClick={handleDecrementMonth}>
-          <ArrowIconLeft width="24px" height="24px" fill="#8B8B8B" />
+          <ArrowIconLeft
+            width="24px"
+            height="24px"
+            fill={theme.COLOR.SECONDARY.DEFAULT}
+          />
         </WrapperButton>
 
         <Styled.Title>
@@ -45,7 +52,11 @@ function Calendar({ channelId, handleCloseCalendar }: Props) {
           isFuture={isFutureMonth()}
           disabled={isFutureMonth()}
         >
-          <ArrowIconRight width="24px" height="24px" fill="#8B8B8B" />
+          <ArrowIconRight
+            width="24px"
+            height="24px"
+            fill={theme.COLOR.SECONDARY.DEFAULT}
+          />
         </WrapperButton>
       </Styled.Month>
 
