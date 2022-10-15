ee6c9cf9e2241af058d20daa307cd612b911885f
bomi <bonita9931@gmail.com>
2022-10-15 23:25:22 +0900

    refactor: 채널 관련 SlackEventServiceTest에서 workspace 추가 및 간단한 리팩토링

--- backend/src/test/java/com/pickpick/slackevent/application/channel/ChannelDeletedServiceTest.java
+++ backend/src/test/java/com/pickpick/slackevent/application/channel/ChannelDeletedServiceTest.java
@@ -1,17 +1,23 @@
 package com.pickpick.slackevent.application.channel;
 
 import static com.pickpick.fixture.ChannelFixture.NOTICE;
+import static com.pickpick.fixture.MemberFixture.HOPE;
+import static com.pickpick.fixture.MessageFixtures.PLAIN_20220712_18_00_00;
+import static com.pickpick.fixture.MessageFixtures.PLAIN_20220715_14_00_00;
+import static com.pickpick.fixture.WorkspaceFixture.JUPJUP;
 import static com.pickpick.support.JsonUtils.toJson;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 import com.pickpick.channel.domain.Channel;
 import com.pickpick.channel.domain.ChannelRepository;
-import com.pickpick.fixture.MessageFixtures;
 import com.pickpick.member.domain.Member;
 import com.pickpick.member.domain.MemberRepository;
 import com.pickpick.message.domain.MessageRepository;
+import com.pickpick.slackevent.application.SlackEvent;
 import com.pickpick.support.DatabaseCleaner;
+import com.pickpick.workspace.domain.Workspace;
+import com.pickpick.workspace.domain.WorkspaceRepository;
 import java.util.Map;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
@@ -32,6 +38,9 @@ class ChannelDeletedServiceTest {
     private MemberRepository members;
 
     @Autowired
+    private WorkspaceRepository workspaces;
+
+    @Autowired
     private ChannelDeletedService channelDeletedService;
 
     @Autowired
@@ -46,19 +55,13 @@ class ChannelDeletedServiceTest {
     @Test
     void channelAndMessagesShouldBeDeletedOnChannelDeletedEvent() {
         // given
-        Member hope = members.save(new Member("U00004", "호프", "https://hope.png"));
+        Workspace jupjup = workspaces.save(JUPJUP.create());
+        Member hope = members.save(HOPE.create(jupjup));
         Channel notice = channels.save(NOTICE.create());
-        messages.save(MessageFixtures.PLAIN_20220712_18_00_00.create(notice, hope));
-        messages.save(MessageFixtures.PLAIN_20220715_14_00_00.create(notice, hope));
+        messages.save(PLAIN_20220712_18_00_00.create(notice, hope));
+        messages.save(PLAIN_20220715_14_00_00.create(notice, hope));
 
-        String request = toJson(
-                Map.of("event",
-                        Map.of(
-                                "type", "channel_deleted",
-                                "channel", notice.getSlackId()
-                        )
-                )
-        );
+        String request = createChannelDeletedRequest(notice);
 
         // when
         channelDeletedService.execute(request);
@@ -67,6 +70,17 @@ class ChannelDeletedServiceTest {
         assertAll(
                 () -> assertThat(channels.findAll()).isEmpty(),
                 () -> assertThat(messages.findAll()).isEmpty()
+        );
+    }
+
+    private String createChannelDeletedRequest(final Channel notice) {
+        return toJson(
+                Map.of("event",
+                        Map.of(
+                                "type", SlackEvent.CHANNEL_DELETED.getType(),
+                                "channel", notice.getSlackId()
+                        )
+                )
         );
     }
 }
--- backend/src/test/java/com/pickpick/slackevent/application/channel/ChannelRenameServiceTest.java
+++ backend/src/test/java/com/pickpick/slackevent/application/channel/ChannelRenameServiceTest.java
@@ -1,5 +1,6 @@
 package com.pickpick.slackevent.application.channel;
 
+import static com.pickpick.fixture.ChannelFixture.NOTICE;
 import static com.pickpick.support.JsonUtils.toJson;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -7,6 +8,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import com.pickpick.channel.domain.Channel;
 import com.pickpick.channel.domain.ChannelRepository;
 import com.pickpick.exception.channel.ChannelNotFoundException;
+import com.pickpick.slackevent.application.SlackEvent;
 import com.pickpick.support.DatabaseCleaner;
 import java.util.Map;
 import org.junit.jupiter.api.AfterEach;
@@ -36,29 +38,31 @@ class ChannelRenameServiceTest {
     @Test
     void channelNameShouldBeChangedOnChannelRenameEvent() {
         // given
-        Channel channel = channels.save(new Channel("slackId", "channelName"));
+        Channel notice = channels.save(NOTICE.create());
+        String newChannelName = "변경된 채널 이름";
 
-        String expectedChannelName = "변경된 채널 이름";
-        String request = toJson(
-                Map.of("event",
-                        Map.of(
-                                "type", "channel_rename",
-                                "channel", Map.of(
-                                        "id", channel.getSlackId(),
-                                        "name", expectedChannelName,
-                                        "created", "1234567890")
-                        )
-                )
-        );
+        String request = createChannelRenamedRequest(notice, newChannelName);
 
         // when
         channelRenameService.execute(request);
 
         // then
-        Channel renamedChannel = channels.findById(channel.getId())
-                .orElseThrow(() -> new ChannelNotFoundException(channel.getId()));
+        Channel renamedChannel = channels.getById(notice.getId());
+        assertThat(renamedChannel.getName()).isEqualTo(newChannelName);
+    }
 
-        assertThat(renamedChannel.getName()).isEqualTo(expectedChannelName);
+    private String createChannelRenamedRequest(final Channel channel, final String newChannelName) {
+        return toJson(
+                Map.of("event",
+                        Map.of(
+                                "type", SlackEvent.CHANNEL_RENAME.getType(),
+                                "channel", Map.of(
+                                        "id", channel.getSlackId(),
+                                        "name", newChannelName,
+                                        "created", "1234567890")
+                        )
+                )
+        );
     }
 
     @DisplayName("채널 이름 변경 이벤트가 전달되었지만 해당 채널이 존재하지 않으면 예외가 발생한다")
