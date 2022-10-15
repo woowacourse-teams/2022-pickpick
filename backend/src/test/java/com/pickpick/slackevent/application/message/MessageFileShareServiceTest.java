a139e58c3ff62991d028301be0a26e56d3b1aeef
bomi <bonita9931@gmail.com>
2022-10-15 23:39:46 +0900

    refactor: MessageFileShareServiceTest 에서 static 제거 및 workspace 추가

--- backend/src/test/java/com/pickpick/slackevent/application/message/MessageFileShareServiceTest.java
+++ backend/src/test/java/com/pickpick/slackevent/application/message/MessageFileShareServiceTest.java
@@ -1,5 +1,9 @@
 package com.pickpick.slackevent.application.message;
 
+import static com.pickpick.fixture.ChannelFixture.QNA;
+import static com.pickpick.fixture.MemberFixture.BOM;
+import static com.pickpick.fixture.MessageFixtures.PLAIN_20220715_17_00_00;
+import static com.pickpick.fixture.WorkspaceFixture.JUPJUP;
 import static com.pickpick.slackevent.application.SlackEvent.MESSAGE_FILE_SHARE;
 import static com.pickpick.support.JsonUtils.toJson;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -7,14 +11,13 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 
 import com.pickpick.channel.domain.Channel;
 import com.pickpick.channel.domain.ChannelRepository;
-import com.pickpick.fixture.ChannelFixture;
-import com.pickpick.fixture.MemberFixture;
 import com.pickpick.member.domain.Member;
 import com.pickpick.member.domain.MemberRepository;
 import com.pickpick.message.domain.Message;
 import com.pickpick.message.domain.MessageRepository;
 import com.pickpick.support.DatabaseCleaner;
-import com.pickpick.utils.TimeUtils;
+import com.pickpick.workspace.domain.Workspace;
+import com.pickpick.workspace.domain.WorkspaceRepository;
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.Optional;
@@ -27,17 +30,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MessageFileShareServiceTest {
-
-    private static final Member SAMPLE_MEMBER = MemberFixture.BOM.create();
-    private static final Channel SAMPLE_CHANNEL = ChannelFixture.QNA.create();
-    private static final Message SAMPLE_MESSAGE = new Message(
-            "db8a1f84-8acf-46ab-b93d-85177cee3e97",
-            "메시지 전송!",
-            SAMPLE_MEMBER,
-            SAMPLE_CHANNEL,
-            TimeUtils.toLocalDateTime("1656919966.864259"),
-            TimeUtils.toLocalDateTime("1656919966.864259")
-    );
 
     @Autowired
     private MessageFileShareService messageFileShareService;
@@ -52,6 +44,9 @@ class MessageFileShareServiceTest {
     private ChannelRepository channels;
 
     @Autowired
+    private WorkspaceRepository workspaces;
+
+    @Autowired
     private DatabaseCleaner databaseCleaner;
 
     @AfterEach
@@ -64,15 +59,18 @@ class MessageFileShareServiceTest {
     @ParameterizedTest
     void saveMessageWhenFileShareEventPassed(final String expectedText) {
         // given
-        members.save(SAMPLE_MEMBER);
-        channels.save(SAMPLE_CHANNEL);
-        Optional<Channel> channelBeforeSave = channels.findBySlackId(SAMPLE_CHANNEL.getSlackId());
-        Optional<Message> messageBeforeSave = messages.findBySlackId(SAMPLE_MESSAGE.getSlackId());
+        Workspace jupjup = workspaces.save(JUPJUP.create());
+        Member bom = members.save(BOM.create(jupjup));
+        Channel qna = channels.save(QNA.create());
+        Message message = PLAIN_20220715_17_00_00.create(qna, bom);
+
+        Optional<Channel> channelBeforeSave = channels.findBySlackId(qna.getSlackId());
+        Optional<Message> messageBeforeSave = messages.findBySlackId(message.getSlackId());
 
         // when
-        messageFileShareService.execute(fileShareRequest(expectedText));
-        Optional<Channel> channelAfterSave = channels.findBySlackId(SAMPLE_CHANNEL.getSlackId());
-        Optional<Message> messageAfterSave = messages.findBySlackId(SAMPLE_MESSAGE.getSlackId());
+        messageFileShareService.execute(fileShareRequest(message, expectedText));
+        Optional<Channel> channelAfterSave = channels.findBySlackId(qna.getSlackId());
+        Optional<Message> messageAfterSave = messages.findBySlackId(message.getSlackId());
 
         // then
         assertAll(
@@ -84,16 +82,16 @@ class MessageFileShareServiceTest {
         );
     }
 
-    private String fileShareRequest(final String text) {
+    private String fileShareRequest(final Message message, final String text) {
         Map<String, Object> request = Map.of("event", Map.of(
                 "type", MESSAGE_FILE_SHARE.getType(),
                 "subtype", MESSAGE_FILE_SHARE.getSubtype(),
                 "files", new ArrayList<>(),
-                "channel", SAMPLE_CHANNEL.getSlackId(),
+                "channel", message.getChannel().getSlackId(),
                 "text", text,
-                "user", SAMPLE_MEMBER.getSlackId(),
+                "user", message.getMember().getSlackId(),
                 "ts", "1656919966.864259",
-                "client_msg_id", SAMPLE_MESSAGE.getSlackId())
+                "client_msg_id", message.getSlackId())
         );
 
         return toJson(request);
