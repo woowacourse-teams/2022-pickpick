e857cf3aac6ab951ca6435ca645d13499e00d434
kkojae <jaejeung3210@gmail.com>
2022-10-20 16:31:28 +0900

    refactor: BookmarkButton, ReminderButton 웹 접근성 description 추가 설정

--- frontend/src/components/MessageCard/MessageIconButtons/BookmarkButton/index.tsx
+++ frontend/src/components/MessageCard/MessageIconButtons/BookmarkButton/index.tsx
@@ -3,7 +3,7 @@ import { MouseEventHandler } from "react";
 import IconButton from "@src/components/@shared/IconButton";
 import StarIcon from "@src/components/@svgIcons/StarIcon";
 
-import { FlexRow } from "@src/@styles/shared";
+import { FlexRow, SrOnlyDescription } from "@src/@styles/shared";
 
 import * as Styled from "../@styles/style";
 
@@ -21,6 +21,12 @@ function BookmarkButton({ isActive, onClick }: Props) {
       onClick={onClick}
       tabIndex={0}
     >
+      <SrOnlyDescription>
+        {isActive
+          ? "북마크가 설정되어 있습니다. 북마크를 해제하시려면 클릭해주세요."
+          : "북마크가 설정되어 있지 않습니다. 북마크를 설정하시려면 클릭해주세요."}
+      </SrOnlyDescription>
+
       <FlexRow justifyContent="center" alignItems="center" gap="5px">
         <Styled.ButtonText>북마크</Styled.ButtonText>
         <StarIcon width="12px" height="13.3px" fill="#ffffff" />
