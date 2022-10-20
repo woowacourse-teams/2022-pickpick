4d3e7d877e77505918c8f6161db38abf972df3cc
moonheekim0118 <hellomooneekim@gmail.com>
2022-10-20 19:42:34 +0900

    fix: messageCard 스토리에 date 파싱 로직 추가

--- frontend/src/components/MessageCard/index.stories.js
+++ frontend/src/components/MessageCard/index.stories.js
@@ -1,3 +1,5 @@
+import { parseMessageDateFromISO } from "@src/@utils/date";
+
 import MessageCard from ".";
 
 export default {
@@ -10,7 +12,7 @@ const Template = (args) => <MessageCard {...args} />;
 export const DefaultTemplate = Template.bind({});
 DefaultTemplate.args = {
   username: "호프(김문희)",
-  date: "2022-07-18T14:50:58.972493",
+  date: parseMessageDateFromISO("2022-07-18T14:50:58.972493"),
   text: "안녕하세요? 저는 우아한테크코스 프론트엔드 4기 호프입니다",
   thumbnail: "https://avatars.githubusercontent.com/u/61469664?v=4",
 };
