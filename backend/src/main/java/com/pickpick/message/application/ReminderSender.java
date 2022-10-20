f421c8d377d567b8acd1f86ba6a5f1a6a963dc95
yeon-06 <rnjstldus2@gmail.com>
2022-10-20 17:26:55 +0900

    refactor: DB의 삭제 로직이 반복문에 영향가지 않도록 수정

--- backend/src/main/java/com/pickpick/message/application/ReminderSender.java
+++ backend/src/main/java/com/pickpick/message/application/ReminderSender.java
@@ -5,12 +5,10 @@ import com.pickpick.message.domain.ReminderRepository;
 import com.pickpick.support.ExternalClient;
 import java.time.LocalDateTime;
 import java.util.List;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-@Slf4j
 @Component
 @Transactional
 public class ReminderSender {
@@ -25,14 +23,19 @@ public class ReminderSender {
 
     @Scheduled(cron = "0 */10 * * * *")
     public void sendRemindMessage() {
-        LocalDateTime now = LocalDateTime.now()
-                .withSecond(0)
-                .withNano(0);
-        List<Reminder> foundReminders = reminders.findAllByRemindDate(now);
+        LocalDateTime nowTime = now();
 
+        List<Reminder> foundReminders = reminders.findAllByRemindDate(nowTime);
         for (Reminder reminder : foundReminders) {
             externalClient.sendMessage(reminder);
-            reminders.deleteById(reminder.getId());
         }
+
+        reminders.deleteAllByRemindDate(nowTime);
+    }
+
+    private LocalDateTime now() {
+        return LocalDateTime.now()
+                .withSecond(0)
+                .withNano(0);
     }
 }
