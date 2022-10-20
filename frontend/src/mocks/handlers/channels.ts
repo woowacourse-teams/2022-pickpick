944dbf25101024e44fbcb10171b3cc331f55f384
moonheekim0118 <hellomooneekim@gmail.com>
2022-10-21 01:14:27 +0900

    feat: channel 구독/구독해제 mock api 작성

--- frontend/src/mocks/handlers/channels.ts
+++ frontend/src/mocks/handlers/channels.ts
@@ -1,6 +1,9 @@
 import { rest } from "msw";
 
 import { channels, subscribedChannels } from "../data/channels";
+import { BODY_TYPE } from "../utils";
+
+let channelsResponse = [...channels];
 
 const handlers = [
   rest.get("/api/channels", (req, res, ctx) => {
@@ -8,7 +11,7 @@ const handlers = [
       ctx.status(200),
       ctx.delay(500),
       ctx.json({
-        channels,
+        channels: channelsResponse,
       })
     );
   }),
@@ -18,9 +21,33 @@ const handlers = [
       ctx.status(200),
       ctx.delay(500),
       ctx.json({
-        channels: subscribedChannels,
+        channels: channelsResponse,
       })
     );
+  }),
+
+  rest.post("/api/channel-subscription", (req, res, ctx) => {
+    const body = req.body as BODY_TYPE;
+    const channelId = Number(body.channelId);
+    channelsResponse = channelsResponse.map((channel) => {
+      if (channel.id === channelId) {
+        return { ...channel, isSubscribed: true };
+      }
+      return channel;
+    });
+
+    return res(ctx.status(200));
+  }),
+
+  rest.delete("/api/channel-subscription", (req, res, ctx) => {
+    const channelId = Number(req.url.searchParams.get("channelId"));
+    channelsResponse = channelsResponse.map((channel) => {
+      if (channel.id === channelId) {
+        return { ...channel, isSubscribed: false };
+      }
+      return channel;
+    });
+    return res(ctx.status(200));
   }),
 ];
 
