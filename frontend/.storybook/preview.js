2d6f51e01c9014689f5a85e686a9f0f76cb2c68c
moonheekim0118 <hellomooneekim@gmail.com>
2022-10-20 17:31:59 +0900

    chore: msw addon 추가하여 스토리북에서 서버 요청보내면 msw 로 보내도록 수정

--- frontend/.storybook/preview.js
+++ frontend/.storybook/preview.js
@@ -1,4 +1,5 @@
 import queryClient from "@src/queryClient";
+import { initialize, mswDecorator } from "msw-storybook-addon";
 import { QueryClientProvider } from "react-query";
 import { MemoryRouter } from "react-router-dom";
 import { RecoilRoot } from "recoil";
@@ -6,6 +7,10 @@ import { ThemeProvider } from "styled-components";
 
 import GlobalStyle from "@src/@styles/GlobalStyle";
 import { LIGHT_MODE_THEME } from "@src/@styles/theme";
+
+initialize({
+  onUnhandledRequest: "bypass",
+});
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -16,8 +21,8 @@ export const parameters = {
     },
   },
 };
-
 export const decorators = [
+  mswDecorator,
   (Story) => (
     <MemoryRouter>
       <RecoilRoot>
@@ -25,7 +30,6 @@ export const decorators = [
           <ThemeProvider theme={LIGHT_MODE_THEME}>
             <GlobalStyle />
             <Story />
-
             <div id="portal-root"></div>
           </ThemeProvider>
         </QueryClientProvider>
@@ -33,3 +37,8 @@ export const decorators = [
     </MemoryRouter>
   ),
 ];
+
+if (typeof global.process === "undefined") {
+  const { worker } = require("../src/mocks/browser");
+  worker.start();
+}
