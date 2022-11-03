2a462014347875d73d517355fd5cfd54c0257781
kkojae <jaejeung3210@gmail.com>
2022-11-03 09:34:58 +0900

    refactor: webpack 불필요한 플러그인 제 및 esbuild-loader로 변경

--- frontend/webpack.prod.js
+++ frontend/webpack.prod.js
@@ -2,22 +2,20 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
 const { merge } = require("webpack-merge");
-const { DefinePlugin } = require("webpack");
 const common = require("./webpack.common.js");
 const { join } = require("path");
+const { ESBuildMinifyPlugin } = require("esbuild-loader");
 
 require("dotenv").config({ path: join(__dirname, "./.env.production") });
 
 module.exports = merge(common, {
   mode: "production",
-  plugins: [
-    new DefinePlugin({
-      "process.env.SLACK_LOGIN_URL": JSON.stringify(
-        process.env.SLACK_LOGIN_URL
-      ),
-      "process.env.SLACK_REGISTER_WORKSPACE_URL": JSON.stringify(
-        process.env.SLACK_REGISTER_WORKSPACE_URL
-      ),
-    }),
-  ],
+  plugins: [],
+  optimization: {
+    minimizer: [
+      new ESBuildMinifyPlugin({
+        target: "esnext",
+      }),
+    ],
+  },
 });
