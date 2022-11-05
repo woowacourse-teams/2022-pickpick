0f19458c9b36e4d65a1e4b69065dcc92d83d99a4
moonheekim0118 <hellomooneekim@gmail.com>
2022-11-05 20:44:47 +0900

    test: 일반 피드 페이지 테스트 작성 - 메시지 첫 렌더링 테스트 - 날짜 드랍다운 렌더링 테스트 - 검색창 포커스 시, 검색 옵션 컴포넌트 렌더링 테스트

--- frontend/src/__test__/FeedPage.test.tsx
+++ frontend/src/__test__/FeedPage.test.tsx
@@ -1,14 +1,37 @@
-import { screen, waitFor } from "@testing-library/react";
-import React from "react";
+import Feed from "@src/pages/Feed";
+import { fireEvent, screen, waitFor } from "@testing-library/react";
 
-import Home from "../pages/Home";
 import { customRender } from "./utils";
 
-describe("일반 피드 페이지 테스트", () => {
-  beforeEach(() => {
-    customRender(<Home />);
+describe("피드 페이지 테스트", () => {
+  beforeEach(async () => {
+    customRender(<Feed />);
   });
-  it("should", async () => {
-    waitFor(() => expect(screen.getByText("hello")).toBeInTheDocument());
+
+  it("사용자는 메시지를 확인 할 수 있다.", async () => {
+    await waitFor(() => {
+      const messages = screen.getAllByRole("listItem");
+      expect(messages.length).toBe(20);
+    });
+  });
+
+  it("사용자는 해당하는 날짜의 드랍다운을 하나 씩 확인 할 수 있다.", async () => {
+    await waitFor(() => {
+      const dateDropdown = screen.getAllByText("7월 18일 월요일");
+      expect(dateDropdown.length).toBe(1);
+    });
+  });
+
+  it("사용자는 검색창을 포커스 할 때, 검색 옵션을 확인 할 수 있다.", async () => {
+    await waitFor(() => {
+      const searchInput =
+        screen.getByPlaceholderText("검색 할 키워드를 입력해주세요.");
+
+      fireEvent.focus(searchInput);
+      const channelList = screen.getByText(
+        "검색에 포함 할 채널을 선택해주세요."
+      );
+      expect(channelList).toBeVisible();
+    });
   });
 });
