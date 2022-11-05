ca5cb55b587554d0766213fd957bb26ef8bb9b88
moonheekim0118 <hellomooneekim@gmail.com>
2022-11-05 22:09:20 +0900

    chore: jest 설정 수정 - resetMocks false 로 수정

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -2,7 +2,6 @@ import type { Config } from "@jest/types";
 
 const config: Config.InitialOptions = {
   bail: 1, // 테스트 1개 실패 할 시 전체 테스트 종료
-  resetMocks: true,
   errorOnDeprecated: true,
 
   rootDir: ".",
